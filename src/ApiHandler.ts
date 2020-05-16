34e2864f96107edfaf024212e2b14f709c729b1d
joepetrowski <joe@parity.io>
2020-05-16 15:50:29 +0200

    on second thought, an if-else

--- src/ApiHandler.ts
+++ src/ApiHandler.ts
@@ -333,8 +333,17 @@ export default class ApiHandler {
 			const blockSpecVersion = version.specVersion;
 			const blockTxVersion = version.transactionVersion;
 
-			// swap metadata if spec version or transaction version is different
-			if (!this.specVersion.eq(blockSpecVersion) || !this.txVersion.eq(blockTxVersion)) {
+			// Swap metadata if tx version is different. This block of code should never execute, as
+			// specVersion always increases when txVersion increases, but specVersion may increase
+			// without an increase in txVersion. Defensive only.
+			if (!this.txVersion.eq(blockTxVersion) && this.specVersion.eq(blockSpecVersion)) {
+				console.warn('txVersion bumped without specVersion bumping');
+				this.txVersion = blockTxVersion;
+				const meta = await api.rpc.state.getMetadata(hash);
+				api.registry.setMetadata(meta);
+			}
+			// Swap metadata and confirm txVersion if specVersion is different.
+			else if (!this.specVersion.eq(blockSpecVersion)) {
 				this.specVersion = blockSpecVersion;
 				this.txVersion = blockTxVersion;
 				const meta = await api.rpc.state.getMetadata(hash);
