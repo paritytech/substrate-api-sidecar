fd58a293f0733581ec5f8a5411225535064b8563
Zeke Mostov <32168567+emostov@users.noreply.github.com>
2020-06-12 11:59:26 -0700

    Add ESLint (#64)
    * Add ESLint
    
    * Silence all warnings and errors
    
    * Fix post merge warnings
    
    Ignore linting calc-fee
    
    New line char

--- src/main.ts
+++ src/main.ts
@@ -14,18 +14,21 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
 import { ApiPromise } from '@polkadot/api';
 import { WsProvider } from '@polkadot/rpc-provider';
-import { parseNumber, sanitizeNumbers } from './utils';
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
+
 import ApiHandler from './ApiHandler';
+import { parseNumber, sanitizeNumbers } from './utils';
 
 const HOST = process.env.BIND_HOST || '127.0.0.1';
 const PORT = Number(process.env.BIND_PORT) || 8080;
 const WS_URL = process.env.NODE_WS_URL || 'ws://127.0.0.1:9944';
 
 type Params = { [key: string]: string };
+
+type TxBody = { tx: string };
 
 async function main() {
 	const api = await ApiPromise.create({ provider: new WsProvider(WS_URL) });
@@ -34,11 +37,13 @@ async function main() {
 
 	app.use(bodyParser.json());
 
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	function get(path: string, cb: (params: Params) => Promise<any>) {
 		app.get(path, async (req, res) => {
 			try {
 				res.send(sanitizeNumbers(await cb(req.params)));
-			} catch(err) {
+			} catch (err) {
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 				if (err && typeof err.error === 'string') {
 					res.status(500).send(sanitizeNumbers(err));
 					return;
@@ -46,17 +51,21 @@ async function main() {
 
 				console.error('Internal Error:', err);
 
-				res.status(500).send({ error: 'Interal Error' });
+				res.status(500).send({ error: 'Internal Error' });
 			}
 		});
 	}
 
-
-	function post(path: string, cb: (params: Params, body: any) => Promise<any>) {
+	function post(
+		path: string,
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		cb: (params: Params, body: any) => Promise<any>
+	) {
 		app.post(path, async (req, res) => {
 			try {
 				res.send(sanitizeNumbers(await cb(req.params, req.body)));
-			} catch(err) {
+			} catch (err) {
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 				if (err && typeof err.error === 'string') {
 					res.status(500).send(sanitizeNumbers(err));
 					return;
@@ -64,12 +73,17 @@ async function main() {
 
 				console.error('Internal Error:', err);
 
-				res.status(500).send({ error: 'Interal Error' });
+				res.status(500).send({ error: 'Internal Error' });
 			}
 		});
 	}
 
-	get('/', async (req) => 'Sidecar is running, go to /block to get latest finalized block');
+	get(
+		'/',
+		// eslint-disable-next-line @typescript-eslint/require-await
+		async () =>
+			'Sidecar is running, go to /block to get latest finalized block'
+	);
 
 	// GET a block.
 	//
@@ -388,14 +402,16 @@ async function main() {
 	// - `query_info`: https://crates.parity.io/pallet_transaction_payment/struct.Module.html#method.query_info
 	// - `compute_fee`: https://crates.parity.io/pallet_transaction_payment/struct.Module.html#method.compute_fee
 	post('/tx/fee-estimate/', async (_, body) => {
-		if(body && typeof body.tx !== 'string'){
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+		if (body && typeof body.tx !== 'string') {
 			return {
-				error: "Missing field `tx` on request body.",
+				error: 'Missing field `tx` on request body.',
 			};
 		}
 
 		const hash = await api.rpc.chain.getFinalizedHead();
 
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 		return await handler.fetchFeeInformation(hash, body.tx);
 	});
 
@@ -415,17 +431,20 @@ async function main() {
 	//   - `data`: The hex-encoded extrinsic. Only present if Sidecar fails to parse a transaction.
 	//   - `cause`: The error message from parsing or from the client.
 	post('/tx/', async (_, body) => {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 		if (body && typeof body.tx !== 'string') {
 			return {
-				error: "Missing field `tx` on request body.",
+				error: 'Missing field `tx` on request body.',
 			};
 		}
 
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 		return await handler.submitTx(body.tx);
 	});
 
-
-	app.listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}/`))
+	app.listen(PORT, HOST, () =>
+		console.log(`Running on http://${HOST}:${PORT}/`)
+	);
 }
 
-main();
+main().catch(console.log);
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import AbstractInt from '@polkadot/types/codec/AbstractInt';
 
 export function parseNumber(n: string): number {
@@ -14,55 +18,56 @@ export function parseNumber(n: string): number {
 // stringified to JSON. We find all instances of `AbstractInt`,
 // which is using bn.js as backend, and forcibly serialize it
 // to a decimal string.
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 export function sanitizeNumbers(data: any): any {
-    if (typeof data === "number" || data instanceof AbstractInt) {
-        return data.toString(10);
-    }
+	if (typeof data === 'number' || data instanceof AbstractInt) {
+		return data.toString(10);
+	}
 
-    if (data instanceof Object) {
-        if (data._raw != null && data._raw instanceof AbstractInt) {
-            return data._raw.toString(10);
-        }
+	if (data instanceof Object) {
+		if (data._raw != null && data._raw instanceof AbstractInt) {
+			return data._raw.toString(10);
+		}
 
-        if (data.raw != null && data.raw instanceof AbstractInt) {
-            return data.raw.toString(10);
-        }
+		if (data.raw != null && data.raw instanceof AbstractInt) {
+			return data.raw.toString(10);
+		}
 
-        if (typeof data.toJSON === "function") {
-            const json = data.toJSON();
+		if (typeof data.toJSON === 'function') {
+			const json = data.toJSON();
 
-            if (Array.isArray(json)) {
-                return data.map(sanitizeNumbers);
-            }
+			if (Array.isArray(json)) {
+				return data.map(sanitizeNumbers);
+			}
 
-            if (json instanceof Object) {
-                const obj = {};
+			if (json instanceof Object) {
+				const obj = {};
 
-                for (const key of Object.keys(json)) {
-                    if (key in data) {
-                        // Prefer un-JSON-ified fields
-                        obj[key] = sanitizeNumbers(data[key]);
-                    } else {
-                        obj[key] = sanitizeNumbers(json[key]);
-                    }
-                }
+				for (const key of Object.keys(json)) {
+					if (key in data) {
+						// Prefer un-JSON-ified fields
+						obj[key] = sanitizeNumbers(data[key]);
+					} else {
+						obj[key] = sanitizeNumbers(json[key]);
+					}
+				}
 
-                return obj;
-            }
+				return obj;
+			}
 
-            return json;
-        }
+			return json;
+		}
 
-        if (Array.isArray(data)) {
-            return data.map(sanitizeNumbers);
-        }
+		if (Array.isArray(data)) {
+			return data.map(sanitizeNumbers);
+		}
 
-        const obj = {};
-        for (const key of Object.keys(data)) {
-            obj[key] = sanitizeNumbers(data[key]);
-        }
-        return obj;
-    }
+		const obj = {};
+		for (const key of Object.keys(data)) {
+			obj[key] = sanitizeNumbers(data[key]);
+		}
+		return obj;
+	}
 
-    return data;
+	return data;
 }
