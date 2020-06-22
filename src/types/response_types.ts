9aaad3f3693bda96e00763ac98949d471e03fa95
Zeke Mostov <z.mostov@gmail.com>
2020-06-21 23:43:05 -0700

    Calc toggle
    initial window change working
    
    clean up
    
    make toggle hardcoded based on chainSpec
    
    make query for activeEra, not derive
    
    save before deleting sessionInfo derive stuff
    
    toggle working; try/catch queueElected;
    
    begin making deriveSessionProgress
    
    deriveSessionProgress done, deleting old
    
    refactor deriveElectionLookAhead, async fetchStakingInfo(hash: BlockHash): Promise<StakingInfo> {
    
    q
    
    add comments and move some things
    
    more refining
    
    correct era progress
    
    clean up
    
    pre squash
    
    
    delete comment

--- src/types/response_types.ts
+++ src/types/response_types.ts
@@ -1,32 +1,22 @@
-/**
- * Response types should closely resemble the actual JSON response given by
- * sidecar. This means they should _not_ have Codec types. It is up to the
- * handler functions to apply the proper serialization of Codec types to ensure
- * they are the expected value. Not only does it gives us more fine grain control
- * of sidecar representation of values in response, this also enables us to
- * handle conversion errors on a case by case basis and give rich, meaningful
- * responses to the client.
- *
- * In sum, there should not be Codec types in any of these responses. Most of
- * the time the value type should either be `string` or `string | null`.
- */
+import { AnyJson } from '@polkadot/types/types';
 
 interface At {
 	hash: string;
-	height: string | number;
+	height: string;
 }
 
-interface StakingInfo {
+export interface StakingInfo {
 	at: At;
 	validatorCount: string; // of validators in the set
 	activeEra: string | null; //ActiveEra.index
-	forceEra: string; // status of era forcing
-	nextEra: string | null;
+	forceEra: AnyJson; // status of era forcing
+	nextEra: AnyJson;
 	nextSession: string | null;
-	unappliedSlashes: string | null;
-	queuedElected: string | null; // contains an array of stashes elected
+	unappliedSlashes: AnyJson[] | null;
+	queuedElected: AnyJson; // contains an array of stashes elected
 	electionStatus: {
-		status: string;
-		toggle: string;
+		status: AnyJson;
+		toggle: string | null;
 	};
+	validatorSet: string[];
 }
