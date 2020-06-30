11d14cecc3aad8d16722bc41080baa65486bd237
Zeke Mostov <z.mostov@gmail.com>
2020-06-30 16:17:42 -0700

    create base and block controller

---
+++ src/controllers/BaseController.ts
@@ -0,0 +1,73 @@
+import { ApiPromise } from '@polkadot/api';
+import { BlockHash } from '@polkadot/types/interfaces';
+import { isHex } from '@polkadot/util';
+import { Router } from 'express';
+import * as express from 'express';
+import { BadRequest } from 'http-errors';
+
+import { parseBlockNumber } from '../utils';
+
+export default abstract class BaseController {
+	private _path: string;
+	private _router: Router = express.Router();
+	protected api: ApiPromise;
+	constructor(api: ApiPromise, path: string) {
+		this.api = api;
+		this._path = path;
+	}
+
+	get path(): string {
+		return this._path;
+	}
+
+	get router(): Router {
+		return this._router;
+	}
+
+	async getHashForBlock(blockId: string): Promise<BlockHash> {
+		let blockNumber;
+
+		try {
+			const isHexStr = isHex(blockId);
+			if (isHexStr && blockId.length === 66) {
+				// This is a block hash
+				return this.api.createType('BlockHash', blockId);
+			} else if (isHexStr) {
+				throw {
+					error:
+						`Cannot get block hash for ${blockId}. ` +
+						`Hex string block IDs must be 32-bytes (66-characters) in length.`,
+				};
+			}
+
+			// Not a block hash, must be a block height
+			try {
+				blockNumber = parseBlockNumber(blockId);
+			} catch (err) {
+				throw {
+					error:
+						`Cannot get block hash for ${blockId}. ` +
+						`Block IDs must be either 32-byte hex strings or non-negative decimal integers.`,
+				};
+			}
+
+			return await this.api.rpc.chain.getBlockHash(blockNumber);
+		} catch (err) {
+			// Check if the block number is too high
+			const { number } = await this.api.rpc.chain.getHeader();
+			if (blockNumber && number.toNumber() < blockNumber) {
+				throw new BadRequest(
+					`Specified block number is larger than the current largest block. ` +
+						`The largest known block number is ${number.toString()}.`
+				);
+			}
+
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+			if (err && typeof err.error === 'string') {
+				throw err;
+			}
+
+			throw { error: `Cannot get block hash for ${blockId}.` };
+		}
+	}
+}
---
+++ src/controllers/BlockController.ts
@@ -0,0 +1,81 @@
+import { ApiPromise } from '@polkadot/api';
+import { BlockHash } from '@polkadot/types/interfaces';
+import { Request, Response } from 'express';
+
+import ApiHandler from '../ApiHandler';
+import BaseController from './BaseController';
+
+export default class BlocksController extends BaseController {
+	handler: ApiHandler;
+	constructor(api: ApiPromise) {
+		super(api, '/block');
+		this.handler = new ApiHandler(api);
+		this.initRoutes();
+	}
+
+	private initRoutes(): void {
+		this.router
+			.get(this.path, this.getLatestBlock)
+			.get(`${this.path}/:number`, this.getBlockById);
+	}
+
+	// GET a block.
+	//
+	// Paths:
+	// - (Optional) `number`: Block hash or height at which to query. If not provided, queries
+	//   finalized head.
+	//
+	// Returns:
+	// - `number`: Block height.
+	// - `hash`: The block's hash.
+	// - `parentHash`: The hash of the parent block.
+	// - `stateRoot`: The state root after executing this block.
+	// - `extrinsicsRoot`: The Merkle root of the extrinsics.
+	// - `authorId`: The account ID of the block author (may be undefined for some chains).
+	// - `logs`: Array of `DigestItem`s associated with the block.
+	// - `onInitialize`: Object with an array of `SanitizedEvent`s that occurred during block
+	//   initialization with the `method` and `data` for each.
+	// - `extrinsics`: Array of extrinsics (inherents and transactions) within the block. Each
+	//   contains:
+	//   - `method`: Extrinsic method, `{module}.{function}`.
+	//   - `signature`: Object with `signature` and `signer`, or `null` if unsigned.
+	//   - `nonce`: Account nonce, if applicable.
+	//   - `args`: Array of arguments.
+	//   - `tip`: Any tip added to the transaction.
+	//   - `hash`: The transaction's hash.
+	//   - `info`: `RuntimeDispatchInfo` for the transaction. Includes the `partialFee`.
+	//   - `events`: An array of `SanitizedEvent`s that occurred during extrinsic execution.
+	//   - `success`: Whether or not the extrinsic succeeded.
+	//   - `paysFee`: Whether the extrinsic requires a fee. Careful! This field relates to whether or
+	//     not the extrinsic requires a fee if called as a transaction. Block authors could insert
+	//     the extrinsic as an inherent in the block and not pay a fee. Always check that `paysFee`
+	//     is `true` and that the extrinsic is signed when reconciling old blocks.
+	// - `onFinalize`: Object with an array of `SanitizedEvent`s that occurred during block
+	//   finalization with the `method` and `data` for each.
+	//
+	// Note: Block finalization does not correspond to consensus, i.e. whether the block is in the
+	// canonical chain. It denotes the finalization of block _construction._
+	//
+	// Substrate Reference:
+	// - `DigestItem`: https://crates.parity.io/sp_runtime/enum.DigestItem.html
+	// - `RawEvent`: https://crates.parity.io/frame_system/enum.RawEvent.html
+	// - Extrinsics: https://substrate.dev/docs/en/knowledgebase/learn-substrate/extrinsics
+	// - `Extrinsic`: https://crates.parity.io/sp_runtime/traits/trait.Extrinsic.html
+	// - `OnInitialize`: https://crates.parity.io/frame_support/traits/trait.OnInitialize.html
+	// - `OnFinalize`: https://crates.parity.io/frame_support/traits/trait.OnFinalize.html
+	private getLatestBlock = async (
+		_req: Request,
+		res: Response
+	): Promise<void> => {
+		const hash = await this.api.rpc.chain.getFinalizedHead();
+		res.send(await this.handler.fetchBlock(hash));
+	};
+
+	private getBlockById = async (
+		req: Request,
+		res: Response
+	): Promise<void> => {
+		const hash: BlockHash = await this.getHashForBlock(req.params.number);
+		res.json(await this.handler.fetchBlock(hash));
+	};
+}
