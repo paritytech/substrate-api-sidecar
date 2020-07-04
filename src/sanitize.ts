21829c958d7072826193eb94b9e1e8c9532a5fd9
Zeke Mostov <z.mostov@gmail.com>
2020-07-03 17:03:27 -0700

    Complete an outline

--- src/sanitize.ts
+++ src/sanitize.ts
@@ -1,17 +1,19 @@
-import { Option, Result, Struct } from '@polkadot/types';
+import { Enum, Option, Result, Struct, Vec } from '@polkadot/types';
+import AbstractArray from '@polkadot/types/codec/AbstractArray';
 import AbstractInt from '@polkadot/types/codec/AbstractInt';
 import { AnyJson, Codec } from '@polkadot/types/types';
 
 /**
- *
+ *Option, Struct, Result, Abstract Array (Tuple, Vec),  Enum,
+ * AbstractInt (Int, UInt)
  * Check
- * Option, Tuple, Vec, Struct, Result, Enum, (Map, Set, Int)
+ * 	Set (BTreeSet), Map (BTreeMap)
+ *
  */
 
 export function sanitizeCodec(value: Codec): AnyJson {
-	if (value instanceof AbstractInt) {
-		return value.toString(10);
-	}
+	// If objects have an overlapping prototype chain
+	// we check lower down the chain first. More specific before less specific.
 
 	// Check option and result before enum because they extend enum
 	if (value instanceof Option) {
@@ -19,7 +21,9 @@ export function sanitizeCodec(value: Codec): AnyJson {
 	}
 
 	if (value instanceof Result) {
-		if (value.isEmpty) {
+		// TODO - I am assuming for an empty OK() we just want null - is this correct?
+		// We could also do `ok: null`
+		if (value.isEmpty || value.isBasic) {
 			return null;
 		}
 
@@ -41,5 +45,30 @@ export function sanitizeCodec(value: Codec): AnyJson {
 		}, {} as Record<string, AnyJson>);
 	}
 
-	return 'stub';
+	// Covers Vecs and Tuples
+	if (value instanceof AbstractArray) {
+		return value.map((element) => sanitizeCodec(element));
+	}
+
+	if (value instanceof Enum) {
+		if (value.isBasic) {
+			return value.toJSON();
+		}
+
+		return { [value.type]: sanitizeCodec(value.value) };
+	}
+
+	if (value instanceof AbstractInt) {
+		return value.toString(10);
+	}
+
+	if (value instanceof Set) {
+		return 'stub';
+	}
+
+	if (value instanceof Map) {
+		return 'stub';
+	}
+
+	return value.toJSON();
 }
