658ee157b06a774962f686b5dcf907632f0a4080
Zeke Mostov <32168567+emostov@users.noreply.github.com>
2020-07-09 16:27:53 -0700

    Breakout main into controllers and app (#134)

--- src/types/request_types.ts
+++ src/types/request_types.ts
@@ -1,13 +1,27 @@
-import { Request } from 'express';
+import { RequestHandler } from 'express';
 import { ParamsDictionary, Query } from 'express-serve-static-core';
 
-export type TxRequestBody = {
+/**
+ * Body for RequestHandlerTx. In other words, the body of a POST route that sends an encoded transaction.
+ */
+export interface ITx {
 	tx: string;
-};
+}
 
-export type TxRequest = Request<
-	ParamsDictionary,
-	unknown,
-	TxRequestBody,
-	Query
->;
+/**
+ * Post Request - assuming no url params
+ */
+// eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
+export type IPostRequestHandler<T> = RequestHandler<{}, any, T, Query>;
+
+export interface INumberParam extends ParamsDictionary {
+	number: string;
+}
+
+export interface IAddressParam extends ParamsDictionary {
+	address: string;
+}
+
+export interface IAddressNumberParams extends IAddressParam {
+	number: string;
+}
