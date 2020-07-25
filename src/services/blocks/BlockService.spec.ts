908fce52cc4d63f5bebb6263cb9b3392947117b4
Zeke Mostov <32168567+emostov@users.noreply.github.com>
2020-07-24 17:18:59 -0700

    Unit test `calc_fee`; Refactor `fetchBlock` (#187)
    * Refactor partial fee calculation to make it testable
    
    Correct name
    
    Create mock api skeletion
    
    save changes
    
    refactor fetch blocks
    
    * Add dummy header
    
    * test createCalcFee
    
    * More refactors of fetchBlock
    
    * Rename constants.ts => mock.ts
    
    * Move mockApi to mock file
    
    * Move header to its own file
    
    * Update commetn
    
    * Update comment

--- src/services/blocks/BlockService.spec.ts
+++ src/services/blocks/BlockService.spec.ts
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { GenericCall } from '@polkadot/types/generic';
+import { Hash } from '@polkadot/types/interfaces';
 
 import { createCall, kusamaRegistry } from '../../utils/testTools';
+import { mockApi, mockBlock789629 } from '../utils/mock';
 import { BlocksService } from './BlocksService';
 
 const transfer = createCall('balances', 'transfer', {
@@ -21,118 +23,154 @@ const transferOutput = {
 	},
 };
 
-describe('BlocksService.parseGenericCall', () => {
-	it('does not handle an empty object', () =>
-		expect(() =>
-			BlocksService['parseGenericCall'](({} as unknown) as GenericCall)
-		).toThrow());
+const blocksService = new BlocksService(mockApi);
 
-	it('parses a simple balances.transfer', () => {
-		expect(
-			JSON.stringify(BlocksService['parseGenericCall'](transfer))
-		).toBe(JSON.stringify(transferOutput));
+describe('BlocksService', () => {
+	describe('createCalcFee & calc_fee', () => {
+		it('calculates partialFee for proxy.proxy in polkadot block 789629', async () => {
+			// tx hash: 0x6d6c0e955650e689b14fb472daf14d2bdced258c748ded1d6cb0da3bfcc5854f
+			const { calcFee } = await blocksService['createCalcFee'](
+				mockApi,
+				('0xParentHash' as unknown) as Hash,
+				mockBlock789629
+			);
+
+			expect(calcFee?.calc_fee(BigInt(399480000), 534)).toBe('544000000');
+		});
+
+		it('calculates partialFee for utility.batch in polkadot block 789629', async () => {
+			// tx hash: 0xc96b4d442014fae60c932ea50cba30bf7dea3233f59d1fe98c6f6f85bfd51045
+			const { calcFee } = await blocksService['createCalcFee'](
+				mockApi,
+				('0xParentHash' as unknown) as Hash,
+				mockBlock789629
+			);
+
+			expect(calcFee?.calc_fee(BigInt(941325000000), 1247)).toBe(
+				'1257000075'
+			);
+		});
 	});
 
-	it('parses utility.batch nested 4 deep', () => {
-		const batch1 = createCall('utility', 'batch', {
-			calls: [transfer],
+	describe('BlocksService.parseGenericCall', () => {
+		it('does not handle an empty object', () =>
+			expect(() =>
+				BlocksService['parseGenericCall'](
+					({} as unknown) as GenericCall
+				)
+			).toThrow());
+
+		it('parses a simple balances.transfer', () => {
+			expect(
+				JSON.stringify(BlocksService['parseGenericCall'](transfer))
+			).toBe(JSON.stringify(transferOutput));
 		});
 
-		const batch2 = createCall('utility', 'batch', {
-			calls: [batch1, transfer],
-		});
+		it('parses utility.batch nested 4 deep', () => {
+			const batch1 = createCall('utility', 'batch', {
+				calls: [transfer],
+			});
 
-		const batch3 = createCall('utility', 'batch', {
-			calls: [batch2, transfer],
-		});
+			const batch2 = createCall('utility', 'batch', {
+				calls: [batch1, transfer],
+			});
 
-		const batch4 = createCall('utility', 'batch', {
-			calls: [batch3, transfer],
-		});
+			const batch3 = createCall('utility', 'batch', {
+				calls: [batch2, transfer],
+			});
 
-		const baseBatch = {
-			method: 'utility.batch',
-			callIndex: new Uint8Array([1, 0]),
-			args: {
-				calls: [],
-			},
-		};
+			const batch4 = createCall('utility', 'batch', {
+				calls: [batch3, transfer],
+			});
 
-		expect(JSON.stringify(BlocksService['parseGenericCall'](batch4))).toBe(
-			JSON.stringify({
-				...baseBatch,
-				args: {
-					calls: [
-						{
-							...baseBatch,
-							args: {
-								calls: [
-									{
-										...baseBatch,
-										args: {
-											calls: [
-												{
-													...baseBatch,
-													args: {
-														calls: [transferOutput],
-													},
-												},
-												transferOutput,
-											],
-										},
-									},
-									transferOutput,
-								],
-							},
-						},
-						transferOutput,
-					],
-				},
-			})
-		);
-	});
-
-	it('handles a batch sudo proxy transfer', () => {
-		const proxy = createCall('proxy', 'proxy', {
-			forceProxyType: 'Any',
-			call: transfer,
-		});
-
-		const sudo = createCall('sudo', 'sudo', {
-			call: proxy,
-		});
-
-		const batch = createCall('utility', 'batch', {
-			calls: [sudo, sudo, sudo],
-		});
-
-		const sudoOutput = {
-			method: 'sudo.sudo',
-			callIndex: new Uint8Array([18, 0]),
-			args: {
-				call: {
-					method: 'proxy.proxy',
-					callIndex: new Uint8Array([28, 0]),
-					args: {
-						real:
-							'5C4hrfjw9DjXZTzV3MwzrrAr9P1MJhSrvWGWqi1eSuyUpnhM',
-						force_proxy_type: 'Any',
-						call: transferOutput,
-					},
-				},
-			},
-		};
-
-		expect(
-			JSON.stringify(BlocksService['parseGenericCall'](batch))
-		).toEqual(
-			JSON.stringify({
+			const baseBatch = {
 				method: 'utility.batch',
 				callIndex: new Uint8Array([1, 0]),
 				args: {
-					calls: [sudoOutput, sudoOutput, sudoOutput],
+					calls: [],
 				},
-			})
-		);
+			};
+
+			expect(
+				JSON.stringify(BlocksService['parseGenericCall'](batch4))
+			).toBe(
+				JSON.stringify({
+					...baseBatch,
+					args: {
+						calls: [
+							{
+								...baseBatch,
+								args: {
+									calls: [
+										{
+											...baseBatch,
+											args: {
+												calls: [
+													{
+														...baseBatch,
+														args: {
+															calls: [
+																transferOutput,
+															],
+														},
+													},
+													transferOutput,
+												],
+											},
+										},
+										transferOutput,
+									],
+								},
+							},
+							transferOutput,
+						],
+					},
+				})
+			);
+		});
+
+		it('handles a batch sudo proxy transfer', () => {
+			const proxy = createCall('proxy', 'proxy', {
+				forceProxyType: 'Any',
+				call: transfer,
+			});
+
+			const sudo = createCall('sudo', 'sudo', {
+				call: proxy,
+			});
+
+			const batch = createCall('utility', 'batch', {
+				calls: [sudo, sudo, sudo],
+			});
+
+			const sudoOutput = {
+				method: 'sudo.sudo',
+				callIndex: new Uint8Array([18, 0]),
+				args: {
+					call: {
+						method: 'proxy.proxy',
+						callIndex: new Uint8Array([28, 0]),
+						args: {
+							real:
+								'5C4hrfjw9DjXZTzV3MwzrrAr9P1MJhSrvWGWqi1eSuyUpnhM',
+							force_proxy_type: 'Any',
+							call: transferOutput,
+						},
+					},
+				},
+			};
+
+			expect(
+				JSON.stringify(BlocksService['parseGenericCall'](batch))
+			).toEqual(
+				JSON.stringify({
+					method: 'utility.batch',
+					callIndex: new Uint8Array([1, 0]),
+					args: {
+						calls: [sudoOutput, sudoOutput, sudoOutput],
+					},
+				})
+			);
+		});
 	});
 });
---
+++ src/services/utils/mock.ts
@@ -0,0 +1,62 @@
+import { ApiPromise } from '@polkadot/api';
+import { Block, Hash } from '@polkadot/types/interfaces';
+
+import { kusamaRegistry } from '../../utils/testTools';
+import { header789629 } from './header78629';
+
+/**
+ * Mock polkadot-js ApiPromise.
+ */
+export const mockApi = ({
+	createType: kusamaRegistry.createType.bind(kusamaRegistry),
+	query: {
+		transactionPayment: {
+			nextFeeMultiplier: {
+				at: (_parentHash: Hash) =>
+					Promise.resolve().then(() =>
+						kusamaRegistry.createType('Fixed128', 1000000000)
+					),
+			},
+		},
+	},
+	consts: {
+		transactionPayment: {
+			transactionByteFee: kusamaRegistry.createType('Balance', 1000000),
+			weightToFee: [
+				{
+					coeffFrac: 80000000,
+					coeffInteger: 0,
+					degree: 1,
+					negative: false,
+				},
+			],
+		},
+		system: {
+			extrinsicBaseWeight: kusamaRegistry.createType('u64', 125000000),
+		},
+	},
+	rpc: {
+		chain: {
+			getHeader: () =>
+				Promise.resolve().then(() => {
+					return kusamaRegistry.createType('Header', header789629);
+				}),
+		},
+		state: {
+			getRuntimeVersion: () =>
+				Promise.resolve().then(() => {
+					return {
+						specName: 'polkadot',
+						specVersion: kusamaRegistry.createType('u32', 16),
+					};
+				}),
+		},
+	},
+} as unknown) as ApiPromise;
+
+/**
+ * Mock for polkadot block #789629
+ */
+export const mockBlock789629 = {
+	header: kusamaRegistry.createType('Header', header789629),
+} as Block;
