e6b344604c3730d5bed2d56b29b8d5ab3c69cbf8
Zeke Mostov <z.mostov@gmail.com>
2020-08-21 10:34:00 -0700

    Update NodeNetwork docs

--- src/controllers/node/NodeNetworkController.ts
+++ src/controllers/node/NodeNetworkController.ts
@@ -4,6 +4,28 @@ import { RequestHandler } from 'express';
 import { NodeNetworkService } from '../../services';
 import AbstractController from '../AbstractController';
 
+/**
+ * GET network information of the node.
+ *
+ * Returns
+ * - `nodeRoles` Roles the node is running.
+ * - `peers` Number of peers the node is connected to.
+ * - `isSyncing` Whether or not the node is syncing. `False` indicates that the
+ * 	node is in sync.
+ * - `shouldHavePeers` Whether or not the node should be connected to peers. Might
+ * 	be false for local chains or when running without discovery.
+ * - `localPeerId` Local copy of the `PeerId`.
+ * - `localListenAddresses` Multiaddresses that the local node is listening on.
+ * 	The addresses include a trailing `/p2p/` with the local PeerId, and are thus
+ *  suitable to be passed to `system_addReservedPeer` or as a bootnode address
+ *  for example.
+ * - `systemPeers` array of
+ * 	- `peerId` Peer ID.
+ *  - `roles` Roles.
+ *  - `protocolVersion` Protocol version.
+ *  - `bestHash` Peer best block hash.
+ *  - `bestNumber` Peer best block number.
+ */
 export default class NodeNetworkController extends AbstractController<
 	NodeNetworkService
 > {
