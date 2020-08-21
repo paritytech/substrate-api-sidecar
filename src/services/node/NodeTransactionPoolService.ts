c94f643886783303ed7d86703b4d2c0d70210f49
Zeke Mostov <z.mostov@gmail.com>
2020-08-21 13:18:29 -0700

    save

--- src/services/node/NodeTransactionPoolService.ts
+++ src/services/node/NodeTransactionPoolService.ts
@@ -1,13 +1,11 @@
-import { Vec } from '@polkadot/types';
-import Extrinsic from '@polkadot/types/extrinsic/Extrinsic';
+// import { Vec } from '@polkadot/types';
+// import Extrinsic from '@polkadot/types/extrinsic/Extrinsic';
 import { BlockHash } from '@polkadot/types/interfaces';
 
 import { AbstractService } from '../AbstractService';
 
 export class NodeTransactionPoolService extends AbstractService {
-	async fetchTransactionPool(
-		hash: BlockHash
-	): Promise<{ pool: Vec<Extrinsic> }> {
+	async fetchTransactionPool(hash: BlockHash): Promise<{ pool: any }> {
 		const api = await this.ensureMeta(hash);
 
 		const pool = await api.rpc.author.pendingExtrinsics();
