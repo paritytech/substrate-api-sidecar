1e8776703ca7db3e6d5c77a74034c4b688612980
Zeke Mostov <z.mostov@gmail.com>
2020-09-05 15:38:03 -0700

    update not implemented endpoints
    save temp
    
    save changes to temp
    
    save
    
    save

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,7 +1,7 @@
 import SwaggerUI from 'swagger-ui';
 import 'swagger-ui/dist/swagger-ui.css';
 
-const spec = require('./openapi-v1-0.yaml');
+const spec = require('./openapi-v1.yaml');
 
 const ui = SwaggerUI({
   spec,
