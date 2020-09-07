53a9dc1b38a62ae940c14bcdbcc0dbbbf50a5b33
Zeke Mostov <32168567+emostov@users.noreply.github.com>
2020-09-07 11:23:39 -0700

    Add OpenApi UI structure in docs folder for future use (#239)

---
+++ docs/src/index.js
@@ -0,0 +1,10 @@
+import SwaggerUI from 'swagger-ui';
+import 'swagger-ui/dist/swagger-ui.css';
+
+const spec = require('./openapi-v1.yaml');
+
+const ui = SwaggerUI({
+  spec,
+  dom_id: '#spec',
+});
+
