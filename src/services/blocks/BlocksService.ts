816831f3c70b2579356ca4ddf31c9cde02067b6a
Zeke Mostov <32168567+emostov@users.noreply.github.com>
2020-09-16 01:32:44 -0700

    Remove irrelevant fields for unsigned extrinsics (#274)
    * Replace test response
    
    * Get test to pass
    
    * Update fields to null

--- src/services/blocks/BlocksService.ts
+++ src/services/blocks/BlocksService.ts
@@ -220,15 +220,16 @@ export class BlocksService extends AbstractService {
 					method: method.methodName,
 				},
 				signature: isSigned ? { signature, signer } : null,
-				nonce,
+				nonce: isSigned ? nonce : null,
 				args: this.parseGenericCall(method).args,
-				tip,
+				tip: isSigned ? tip : null,
 				hash,
 				info: {},
 				events: [] as ISanitizedEvent[],
 				success: defaultSuccess,
 				// paysFee overrides to bool if `system.ExtrinsicSuccess|ExtrinsicFailed` event is present
-				paysFee: null as null | boolean,
+				// we set to false if !isSigned because unsigned never pays a fee
+				paysFee: isSigned ? null : false,
 				docs: extrinsicDocs
 					? this.sanitizeDocs(extrinsic.meta.documentation)
 					: undefined,
@@ -289,7 +290,7 @@ export class BlocksService extends AbstractService {
 						const sanitizedData = event.data.toJSON() as AnyJson[];
 
 						for (const data of sanitizedData) {
-							if (isPaysFee(data)) {
+							if (extrinsic.signature && isPaysFee(data)) {
 								extrinsic.paysFee =
 									data.paysFee === true ||
 									data.paysFee === 'Yes';
