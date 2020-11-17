fd48eb0a00dabc2ad2073d27bf4be75880bee61a
Zeke Mostov <32168567+emostov@users.noreply.github.com>
2020-11-16 16:38:15 -0800

    Use tabs not spaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json'
-  },
-  plugins: [
-    '@typescript-eslint',
-    'prettier',
-    'simple-import-sort'
-  ],
-  rules: {
-    // Sort imports
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'prettier/@typescript-eslint',
+		'plugin:prettier/recommended'
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: './tsconfig.json'
+	},
+	plugins: [
+		'@typescript-eslint',
+		'prettier',
+		'simple-import-sort'
+	],
+	rules: {
+		// Sort imports
 		'simple-import-sort/imports': 'error',
 		'simple-import-sort/exports': 'error',
-    // https://github.com/eslint/eslint/issues/2321#issuecomment-134665757
-    'no-unused-vars': [2, { args: 'all', argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-unused-vars': [
-      2,
-      { args: 'all', argsIgnorePattern: '^_' }
-    ],
-    "@typescript-eslint/ban-types": 0,
-  },
+		// https://github.com/eslint/eslint/issues/2321#issuecomment-134665757
+		'no-unused-vars': [2, { args: 'all', argsIgnorePattern: '^_' }],
+		'@typescript-eslint/no-unused-vars': [
+			2,
+			{ args: 'all', argsIgnorePattern: '^_' }
+		],
+		"@typescript-eslint/ban-types": 0,
+	},
 };
