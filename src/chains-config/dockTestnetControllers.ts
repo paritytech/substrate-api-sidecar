00919e5b105693e740ad4fd5352f1a7ba7fe7275
lovesh <lovesh.bond@gmail.com>
2020-12-15 19:38:56 +0530

    Add controllers for Dock
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

---
+++ src/chains-config/dockTestnetControllers.ts
@@ -0,0 +1,29 @@
+import { ControllerConfig } from '../types/chains-config';
+
+/**
+ * Controllers for Dock's test network.
+ */
+export const dockTestnetControllers: ControllerConfig = {
+	controllers: {
+		Blocks: true,
+		AccountsStakingPayouts: false,
+		AccountsBalanceInfo: true,
+		AccountsStakingInfo: false,
+		AccountsVestingInfo: false,
+		NodeNetwork: true,
+		NodeVersion: true,
+		NodeTransactionPool: true,
+		RuntimeCode: true,
+		RuntimeSpec: true,
+		RuntimeMetadata: true,
+		TransactionDryRun: true,
+		TransactionMaterial: true,
+		TransactionFeeEstimate: true,
+		TransactionSubmit: true,
+		PalletsStakingProgress: false,
+		PalletsStorage: true,
+	},
+	options: {
+		finalizes: true,
+	},
+};
