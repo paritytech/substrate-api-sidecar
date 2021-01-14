60ac96a614387cf7e55d5144d99043240d0a0d33
Tarik <tariksnow37@gmail.com>
2021-01-14 00:28:40 -0500

    Add new /blocks/{blockId}/extrinsics/{extrinsicsIndex} endpoint

---
+++ src/controllers/blocks/BlocksExtrinsicsController.ts
@@ -0,0 +1,49 @@
+import { ApiPromise } from '@polkadot/api';
+import { RequestHandler } from 'express';
+
+import { BlocksService } from '../../services';
+import { INumberParam } from '../../types/requests';
+import AbstractController from '../AbstractController';
+
+export default class BlocksExtrinsicsController extends AbstractController<BlocksService> {
+    constructor(api: ApiPromise) {
+        super(
+            api, 
+            '/blocks/:blockId/extrinsics', 
+            new BlocksService(api)
+        );
+        this.initRoutes();
+    }
+
+    protected initRoutes(): void {
+        this.safeMountAsyncGetHandlers([
+            ['/:extrinsicsIndex', this.getExtrinsicsByExtrsinsicIndex]
+        ])
+    }
+
+    /**
+     * 
+     * @param _req Express Request
+     * @param res Express Response
+     */
+    private getExtrinsicsByExtrsinsicIndex: RequestHandler<INumberParam> = async (
+        { params: { blockId, extrinsicsIndex }, query: { eventDocs, extrinsicDocs } },
+        res
+    ): Promise<void> => {
+        const hash = await this.getHashForBlock(blockId);
+
+        const eventDocsArg = eventDocs === 'true';
+        const extrinsinsicDocsArg = extrinsicDocs === 'true';
+
+        let block = await this.service.fetchBlock(
+            hash,
+            eventDocsArg,
+            extrinsinsicDocsArg
+        )
+
+        BlocksExtrinsicsController.sanitizedSend(
+            res,
+            block.extrinsics[extrinsicsIndex],
+        )
+    }
+}
