877868691585107fd6fa75e0af61d3204f579327
emostov <32168567+emostov@users.noreply.github.com>
2021-01-27 15:00:37 -0800

    Collect and test usage data of querys

--- src/services/accounts/AccountsBalanceInfoService.spec.ts
+++ src/services/accounts/AccountsBalanceInfoService.spec.ts
@@ -1,7 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { AccountInfo, Address, Hash } from '@polkadot/types/interfaces';
 
 import { sanitizeNumbers } from '../../sanitize/sanitizeNumbers';
@@ -25,37 +24,90 @@ describe('AccountsBalanceInfoService', () => {
 			).toStrictEqual(accountsBalanceInfo789629);
 		});
 
-		it('does not change the casing of non-native tokens', async () => {
-			// `temp` should just be `undefined` but to future proof this it is safer
-			// to reassign this back to what ever it was at the start of the test (see
-			// last line of this test)
-			const temp = mockApi.query.tokens;
+		describe('token recognization', () => {
+			let tempQueryTokens: any,
+				tempQueryBalance: any,
+				mockTokensLocksAt: jest.Mock<any>,
+				mockTokenAccountAt: jest.Mock<any>,
+				mockBalancesLocksAt: jest.Mock<any>;
+			beforeAll(() => {
+				// Import: these temp values should never be reassinged. These are used
+				// so we can assign these back to there orignal values after this sub-section
+				// of tests run.
+				tempQueryTokens = mockApi.query.tokens;
+				tempQueryBalance = mockApi.query.balances;
 
-			const tokensAccountAt = async (
-				hash: Hash,
-				address: Address
-			): Promise<any> =>
-				(((await mockApi.query.system.account.at(
-					hash,
-					address
-				)) as unknown) as AccountInfo).data;
-			mockApi.query.tokens = {
-				locks: { at: mockApi.query.balances.locks.at },
-				accounts: { at: tokensAccountAt },
-			} as any;
+				const tokensAccountAt = async (
+					hash: Hash,
+					address: Address
+				): Promise<any> =>
+					(((await mockApi.query.system.account.at(
+						hash,
+						address
+					)) as unknown) as AccountInfo).data;
+				// Wrap our functions in a jest mock so we can collect data on how they where called
+				mockTokensLocksAt = jest.fn(mockApi.query.balances.locks.at);
+				mockTokenAccountAt = jest.fn(tokensAccountAt);
+				mockApi.query.tokens = {
+					locks: { at: mockTokensLocksAt },
+					accounts: { at: mockTokenAccountAt },
+				} as any;
 
-			expect(
-				(sanitizeNumbers(
-					await accountsBalanceInfoService.fetchAccountBalanceInfo(
-						blockHash789629,
-						testAddress,
-						'ausd'
-					)
-				) as any).tokenSymbol
-			).toEqual('ausd');
+				mockBalancesLocksAt = jest.fn(mockApi.query.balances.locks.at);
+				mockApi.query.balances = {
+					locks: { at: mockBalancesLocksAt },
+				} as any;
+			});
 
-			// Cleanup
-			mockApi.query.tokens = temp;
+			afterEach(() => {
+				// Clear data about how the mocks where called after each `it` test.
+				mockTokensLocksAt.mockClear();
+				mockTokenAccountAt.mockClear();
+				mockBalancesLocksAt.mockClear();
+			});
+
+			afterAll(() => {
+
+				mockApi.query.tokens = tempQueryTokens;
+				mockApi.query.balances = tempQueryBalance;
+			});
+
+			it('only has `DOT` (all uppercase) for the mockApi registry', () => {
+				expect(mockApi.registry.chainTokens).toStrictEqual(['DOT']);
+				expect(mockApi.registry.chainDecimals).toStrictEqual([12]);
+			});
+
+			it('querrys tokens pallet storage with a non-native token', async () => {
+				expect(
+					(sanitizeNumbers(
+						await accountsBalanceInfoService.fetchAccountBalanceInfo(
+							blockHash789629,
+							testAddress,
+							'fOoToKeN'
+						)
+					) as any).tokenSymbol
+				).toEqual('fOoToKeN');
+
+				expect(mockTokensLocksAt).toBeCalled();
+				expect(mockTokenAccountAt).toBeCalled();
+				expect(mockBalancesLocksAt).not.toBeCalled();
+			});
+
+			it('does not query tokens pallet storage with the native token', async () => {
+				expect(
+					(sanitizeNumbers(
+						await accountsBalanceInfoService.fetchAccountBalanceInfo(
+							blockHash789629,
+							testAddress,
+							'doT'
+						)
+					) as any).tokenSymbol
+				).toEqual('doT');
+
+				expect(mockTokensLocksAt).not.toBeCalled();
+				expect(mockTokenAccountAt).not.toBeCalled();
+				expect(mockBalancesLocksAt).toBeCalled();
+			});
 		});
 	});
 });
