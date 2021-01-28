35156ce5cdac6adad09769d8662a91e7a17c8151
emostov <32168567+emostov@users.noreply.github.com>
2021-01-28 08:10:32 -0800

    Remove unused type

--- src/services/test-helpers/mock/index.ts
+++ src/services/test-helpers/mock/index.ts
@@ -1,16 +1,4 @@
-import { Hash } from '@polkadot/types/interfaces';
-import { Codec, CodecArg } from '@polkadot/types/types';
-
 export * from './addresses';
 export * from './mockApi';
 export * from './mockBlock789629';
 export * from './transactions';
-
-/**
- * Convience type for type casting mockApi storage querys.
- */
-export type PolkadotStorageQuery = <T extends unknown = Codec>(
-	hash: string | Hash | Uint8Array,
-	arg1?: CodecArg,
-	arg2?: CodecArg
-) => Promise<T>;
