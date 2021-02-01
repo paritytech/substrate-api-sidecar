3d77f75cb071257221c0ad9e3c0e096890b8bd0b
tarikgul <tarik@parity.io>
2021-02-01 15:46:24 -0500

    fix: update test to take in extrinsicBaseweight param

--- src/services/blocks/BlocksService.spec.ts
+++ src/services/blocks/BlocksService.spec.ts
@@ -124,7 +124,7 @@ describe('BlocksService', () => {
 				mockBlock789629
 			);
 
-			expect(calcFee?.calc_fee(BigInt(399480000), 534)).toBe('544000000');
+			expect(calcFee?.calc_fee(BigInt(399480000), 534, BigInt(125000000))).toBe('544000000');
 		});
 
 		it('calculates partialFee for utility.batch in polkadot block 789629', async () => {
@@ -135,7 +135,7 @@ describe('BlocksService', () => {
 				mockBlock789629
 			);
 
-			expect(calcFee?.calc_fee(BigInt(941325000000), 1247)).toBe(
+			expect(calcFee?.calc_fee(BigInt(941325000000), 1247, BigInt(125000000))).toBe(
 				'1257000075'
 			);
 		});
