d6f43a0c498e96378591fd72d65fdeebcc07fe44
Zeke Mostov <32168567+emostov@users.noreply.github.com>
2021-02-23 13:12:51 -0800

    build(linting): Update prettier and eslint (#447)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:@typescript-eslint/recommended-requiring-type-checking',
-		'prettier/@typescript-eslint',
+		'prettier',
 		'plugin:prettier/recommended'
 	],
 	parser: '@typescript-eslint/parser',
