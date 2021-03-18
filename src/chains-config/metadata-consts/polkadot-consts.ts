67f1cd611707dc55d7e74abc8b221c26cdc6a949
tarikgul <tarik@parity.io>
2021-03-18 16:44:15 -0400

    fix: appropiate weights to each chains file

--- src/chains-config/metadata-consts/polkadot-consts.ts
+++ src/chains-config/metadata-consts/polkadot-consts.ts
@@ -1,5 +1,37 @@
 import { Definition } from '../../types/chains-config';
-import { blockWeights, extrinsicBaseWeight } from './substrate-consts';
+
+/**
+ * All Runtimes before v27
+ */
+const extrinsicBaseWeight = 125000000;
+
+/**
+ * Runtimes after v26
+ */
+const blockWeights = {
+	baseBlock: 5000000000,
+	maxBlock: 2000000000000,
+	perClass: {
+		normal: {
+			baseExtrinsic: 125000000,
+			maxExtrinsic: 371168000000001,
+			maxTotal: null,
+			reserved: 1500000000000,
+		},
+		operational: {
+			baseExtrinsic: 1,
+			maxExtrinsic: 32000000000,
+			maxTotal: null,
+			reserved: 1949875000000,
+		},
+		mandatory: {
+			baseExtrinsic: 512000000000001,
+			maxExtrinsic: '0x00746a5288000100',
+			maxTotal: null,
+			reserved: null,
+		},
+	},
+};
 
 export const polkadotDefinitions: Definition[] = [
 	{
