7452313ca818187ea319df1cf4974cec13d4fffd
tarikgul <tarik@parity.io>
2021-03-23 18:46:19 -0400

    fix: fix parsing of data in generateBlockWeightObject, cleanup types, rename items

--- src/chains-config/metadata-consts/index.ts
+++ src/chains-config/metadata-consts/index.ts
@@ -1,22 +1,40 @@
-import { MetadataConstDefinition } from '../../types/chains-config';
+import {
+	MetadataConstDefinition,
+	MetadataWeightDefinition,
+} from '../../types/chains-config';
 import { kusamaDefinitions } from './kusamaConsts';
 import { polkadotDefinitions } from './polkadotConsts';
 
 /**
- * Creates an object that orders each runtime to their appropriate data
+ * Creates an object that orders each runtime to their appropriate weight data.
  *
+ * Example return object:
+ * {
+ * 	   ...
+ *     '24': { extrinsicBaseWeight: 125000000 },
+ *     '25': { extrinsicBaseWeight: 125000000 },
+ *     '26': { extrinsicBaseWeight: 125000000 },
+ *     '27': { blockWeights: { perClass: [Object] } },
+ *     '28': { blockWeights: { perClass: [Object] } }
+ *     ...
+ * }
  * @param definitions An array of objects that group data based on runtimes
  * and their extrinsicBaseWeight metadata
  */
 const generateBlockWeightObject = (
-	definitions: MetadataConstDefinition[]
-): Record<string, MetadataConstDefinition> => {
-	const blockWeightObject = {};
+	chainDefinitions: MetadataConstDefinition[]
+): Record<string, MetadataWeightDefinition> => {
+	const blockWeightObject: Record<string, MetadataWeightDefinition> = {};
 
-	for (const def of definitions) {
+	for (const def of chainDefinitions) {
 		const runtimeVersions: number[] = def.runtimeVersions;
 		for (const version of runtimeVersions) {
-			blockWeightObject[version] = def;
+			const weightType: string = def.extrinsicBaseWeight
+				? 'extrinsicBaseWeight'
+				: `blockWeights`;
+			blockWeightObject[version] = {} as MetadataConstDefinition;
+			blockWeightObject[version][weightType] =
+				def.blockWeights || def.extrinsicBaseWeight;
 		}
 	}
 
@@ -31,7 +49,7 @@ const generateBlockWeightObject = (
  */
 export const getBlockWeight = (
 	specName: string
-): Record<string, MetadataConstDefinition> => {
+): Record<string, MetadataWeightDefinition> => {
 	switch (specName) {
 		case 'polkadot':
 			return generateBlockWeightObject(polkadotDefinitions);
--- src/types/chains-config/MetadataConsts.ts
+++ src/types/chains-config/MetadataConsts.ts
@@ -1,14 +1,19 @@
-export interface MetadataConstDefinition {
+export interface MetadataConstDefinition extends MetadataWeightDefinition {
 	/**
 	 * Runtimes that include their specific definition of extrinsicBaseWeight
 	 */
 	runtimeVersions: number[];
+}
+
+export interface MetadataWeightDefinition {
 	/**
-	 * Runtime < v27
+	 * Polkadot runtime versions before v0.8.27
+	 * Kusama runtime versions before v2027
 	 */
 	extrinsicBaseWeight?: number;
 	/**
-	 * Runtime >= 27
+	 * Polkadot runtime versions after v0.8.26
+	 * Kusama runtime versions after v2026
 	 */
 	blockWeights?: BlockWeightDefinitions;
 }
