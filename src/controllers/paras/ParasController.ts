04aa5c7067dd2f0e5c5fe1d2b42a48e8dab9d02b
tarikgul <tarik@parity.io>
2021-04-02 16:54:21 -0400

    cleanup service and controller

--- src/controllers/paras/ParasController.ts
+++ src/controllers/paras/ParasController.ts
@@ -14,26 +14,9 @@ export default class ParasController extends AbstractController<ParasService> {
 	protected initRoutes(): void {
 		this.safeMountAsyncGetHandlers([
 			['/crowdloans', this.getCrowdloans],
-			['/:paraId/lease-info', this.getLeaseInfo],
 			['/:paraId/crowdloans-info', this.getCrowdloansInfo],
 		]);
 	}
-
-	private getLeaseInfo: RequestHandler<IParaIdParam> = async (
-		{ params: { paraId }, query: { at } },
-		res
-	): Promise<void> => {
-		const hash = await this.getHashFromAt(at);
-		const paraIdArg = this.parseNumberOrThrow(
-			paraId,
-			'paraId must be an integer'
-		);
-
-		ParasController.sanitizedSend(
-			res,
-			await this.service.leaseInfo(hash, paraIdArg)
-		);
-	};
 
 	private getCrowdloansInfo: RequestHandler<IParaIdParam> = async (
 		{ params: { paraId }, query: { at } },
--- src/services/paras/ParasService.ts
+++ src/services/paras/ParasService.ts
@@ -1,89 +1,15 @@
-import { Option, Tuple, Vec } from '@polkadot/types/codec';
-import { AbstractInt } from '@polkadot/types/codec/AbstractInt';
-import {
-	AccountId,
-	BlockHash,
-	FundInfo,
-	ParaId,
-} from '@polkadot/types/interfaces';
-import BN from 'bn.js';
+import { Option } from '@polkadot/types/codec';
+import { BlockHash, FundInfo, ParaId } from '@polkadot/types/interfaces';
 import { InternalServerError } from 'http-errors';
 
-import { IAt } from '../../types/responses';
+import {
+	ICrowdloansInfoResponse,
+	ICrowdloansResponse,
+	IEntries,
+} from '../../types/responses';
 import { AbstractService } from '../AbstractService';
 
-/**
- * Wrote the types up here for now just for development, will move to
- * types directory when this is wrapped up
- */
-interface IEntries {
-	paraId: number;
-	fundInfo: Option<FundInfo> | {};
-}
-
-interface ICrowdloansInfoResponse {
-	at: IAt;
-	fundInfo: Option<FundInfo>;
-}
-
-interface ICrowdloansResponse {
-	at: IAt;
-	funds: IEntries[];
-}
-
 export class ParasService extends AbstractService {
-	async leaseInfo(hash: BlockHash, paraId: number): Promise<any> {
-		const [leasesAt, { number }] = await Promise.all([
-			this.api.query.slots.leases.at(hash, paraId),
-			this.api.rpc.chain.getHeader(hash),
-		]);
-		const leasesTyped = leasesAt as Vec<Option<Tuple>>;
-		const blockNumber = number.unwrap();
-
-		const at = {
-			hash,
-			height: blockNumber.toString(10),
-		};
-
-		if (!leasesTyped.length) {
-			return {
-				at,
-				leases: [],
-				message: `Para with ID {paraId} no longer exists or never has existed`,
-			};
-		}
-
-		const leasePeriod = this.api.consts.slots.leasePeriod as AbstractInt;
-
-		// Caluclate current lease period index
-		const currentLeasePeriodIndex = this.currentLeasePeriodIndex(
-			blockNumber,
-			leasePeriod
-		);
-
-		const leases = leasesTyped.map((leaseOpt, idx) => {
-			const leasePeriodIndex = currentLeasePeriodIndex + idx;
-
-			if (leaseOpt.isSome) {
-				const lease = leaseOpt.unwrap();
-				return {
-					leasePeriodIndex,
-					deposit: lease[1] as AbstractInt,
-					account: lease[0] as AccountId,
-				};
-			}
-
-			return {
-				leasePeriodIndex,
-				// TODO: should these be omitted completely or have them as null?
-				deposit: null,
-				account: null,
-			};
-		});
-
-		return { at, leases };
-	}
-
 	/**
 	 * @param hash
 	 * @param paraId
@@ -151,7 +77,7 @@ export class ParasService extends AbstractService {
 		).map((k) => {
 			const paraId = (k[0].args[0] as ParaId).toNumber();
 			const funds: Option<FundInfo> | {} = includeFundInfo
-				? k[1] as Option<FundInfo>
+				? (k[1] as Option<FundInfo>)
 				: {};
 
 			return {
@@ -169,9 +95,5 @@ export class ParasService extends AbstractService {
 			at,
 			funds: entries,
 		};
-	}
-
-	private currentLeasePeriodIndex(blockHeight: BN, leasePeriod: BN): number {
-		return blockHeight.div(leasePeriod).toNumber();
 	}
 }
