bbb696fe70f6715070695ddbc7e736b3912bcf29
tarikgul <tarik@parity.io>
2021-04-01 23:26:10 -0400

    add crowdloan routes to the ParasController

--- src/controllers/paras/ParasController.ts
+++ src/controllers/paras/ParasController.ts
@@ -13,7 +13,9 @@ export default class ParasController extends AbstractController<ParasService> {
 
 	protected initRoutes(): void {
 		this.safeMountAsyncGetHandlers([
+			['/crowdloans', this.getCrowdloans],
 			['/:paraId/lease-info', this.getLeaseInfo],
+			['/:paraId/crowdloans-info', this.getCrowdloansInfo],
 		]);
 	}
 
@@ -30,6 +32,36 @@ export default class ParasController extends AbstractController<ParasService> {
 		ParasController.sanitizedSend(
 			res,
 			await this.service.leaseInfo(hash, paraIdArg)
+		);
+	};
+
+	private getCrowdloansInfo: RequestHandler<IParaIdParam> = async (
+		{ params: { paraId }, query: { at } },
+		res
+	): Promise<void> => {
+		const hash = await this.getHashFromAt(at);
+		const paraIdArg = this.parseNumberOrThrow(
+			paraId,
+			'paraId must be an integer'
+		);
+
+		ParasController.sanitizedSend(
+			res,
+			await this.service.crowdloansInfo(hash, paraIdArg)
+		);
+	};
+
+	private getCrowdloans: RequestHandler = async (
+		{ query: { at, fundInfo } },
+		res
+	): Promise<void> => {
+		const hash = await this.getHashFromAt(at);
+
+		const includeFundInfo = fundInfo === 'false' ? false : true;
+
+		ParasController.sanitizedSend(
+			res,
+			await this.service.crowdloans(hash, includeFundInfo)
 		);
 	};
 }
