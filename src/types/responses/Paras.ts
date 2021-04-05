b898e3da1818ae78cdd5bd1be9cdf7ef23e77175
Zeke Mostov <32168567+emostov@users.noreply.github.com>
2021-04-05 12:18:24 -0700

    Add endpoints (#504)
    * Update auctions current to use offset to key into Winning
    
    * MVP leases current
    
    * Add paras service
    
    * Add /paras to controller
    
    * Update doc comments
    
    * Add retirement end
    
    * Add response interfaces
    
    * Add interfaces for leases, para, and auctions
    
    * Add doc comment to interface
    
    * Exclude lease-info for indexs with no lease
    
    * Remove retirement period end bc no longer have retirements
    
    * Address tarik review comment
    
    * Apply suggestions from code review
    
    Co-authored-by: Tarik Gul <47201679+TarikGul@users.noreply.github.com>
    
    Co-authored-by: Tarik Gul <47201679+TarikGul@users.noreply.github.com>

--- src/types/responses/Paras.ts
+++ src/types/responses/Paras.ts
@@ -1,19 +1,154 @@
 import { Option } from '@polkadot/types/codec';
-import { FundInfo, ParaId } from '@polkadot/types/interfaces';
+import { AbstractInt } from '@polkadot/types/codec/AbstractInt';
+import {
+	AccountId,
+	BalanceOf,
+	FundInfo,
+	ParaId,
+	ParaLifecycle,
+	WinningData,
+} from '@polkadot/types/interfaces';
+import BN from 'bn.js';
 
+import { IOption } from '../util';
 import { IAt } from './';
 
-export interface IEntries {
-    paraId: ParaId;
+export type AuctionPhase = 'preEnding' | 'ending';
+
+export type ParaType = 'parachain' | 'parathread';
+
+export interface IFund {
+	/**
+	 * Id of the parachain or parathread that has a crowdloan.
+	 */
+	paraId: ParaId;
+	/**
+	 * `FundInfo` for the para's crowdloan. Use query param `fundInfo=true` to include the `FundInfo`.
+	 * Inclusion may make the query take longer.
+	 */
 	fundInfo?: Option<FundInfo>;
 }
 
-export interface ICrowdloansInfoResponse {
+export interface ICrowdloansInfo {
 	at: IAt;
-	fundInfo: Option<FundInfo>;
+	/**
+	 * `FundInfo` describing the crowdloan, or null if none could be found.
+	 */
+	fundInfo: IOption<FundInfo>;
+	/**
+	 * Lease period indexes that the crowdloan applys to.
+	 */
+	leasePeriods?: number[];
 }
 
-export interface ICrowdloansResponse {
+export interface ICrowdloans {
 	at: IAt;
-	funds: IEntries[];
+	/**
+	 * List of all crowdloans.
+	 */
+	funds: IFund[];
+}
+
+export interface LeaseFormatted {
+	/**
+	 * Lease period. (Represents a ~6 month period that a parachain holds a lease for.)
+	 */
+	leasePeriodIndex: number;
+	/**
+	 * Amount held on deposit for parachain
+	 */
+	deposit: BalanceOf;
+	/**
+	 * Account responsible for the lease.
+	 */
+	account: AccountId;
+}
+
+export interface ILeaseInfo {
+	at: IAt;
+	/**
+	 * Lifecycle of the para (i.e Onboarding, Parathread, Offboarding etc)
+	 */
+	paraLifeCycle: ParaLifecycle;
+	/**
+	 * If the para is in the onboarding phase, this will say if it is onboarding as
+	 * a `parachain` or a `parathread`.
+	 */
+	onboardingAs?: ParaType;
+	/**
+	 * List of current and upcoming leases this para has.
+	 */
+	leases: IOption<LeaseFormatted[]>;
+}
+
+export interface IAuctionsCurrent {
+	at: IAt;
+	/**
+	 * Fist block of the auction ending phase.
+	 */
+	beginEnd: IOption<AbstractInt>;
+	/**
+	 * Last block of the auction ending phase.
+	 */
+	finishEnd: IOption<BN>;
+	/**
+	 * Phase of auction. One of `PreEnding` or `Ending`. The `Ending` phase is where
+	 * an eventual winner is chosen retroactively by randomly choosing a block number
+	 * in the `Ending` phase and using the `winning` bids.
+	 */
+	phase: IOption<AuctionPhase>;
+	/**
+	 * Auction number. If there is no current auction this will be the number of
+	 * the previous auction.
+	 */
+	auctionIndex: AbstractInt;
+	/**
+	 * Lease period indexes that may be bid on in this auction.
+	 */
+	leasePeriods: IOption<number[]>;
+	/**
+	 * Winning bids at this current block height. Is only not `null` during the
+	 * `Ending` phase.
+	 */
+	winning: IOption<Option<WinningData>>;
+}
+
+export interface ILeasesCurrent {
+	at: IAt;
+	/**
+	 * The current lease period.
+	 */
+	leasePeriodIndex: BN;
+	/**
+	 * Last block of the current lease period.
+	 */
+	endOfLeasePeriod: BN;
+	/**
+	 * ParaIds of current lease holders.
+	 */
+	currentLeaseHolders?: ParaId[];
+}
+
+export interface IPara {
+	/**
+	 * Id of a parachain or parathread
+	 */
+	paraId: ParaId;
+	/**
+	 * Lifecycle stage of the para.
+	 */
+	paraLifeCycle: ParaLifecycle;
+	/**
+	 * If the para is in the `onboarding` lifecycle stage, this will indicate if
+	 * the para is onboarding as a parachain or a parathread.
+	 */
+	onboardingAs?: ParaType;
+}
+
+export interface IParas {
+	at: IAt;
+	/**
+	 * All registered paras.
+	 */
+	paras: IPara[];
 }
