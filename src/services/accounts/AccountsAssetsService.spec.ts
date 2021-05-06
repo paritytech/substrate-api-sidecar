8f56a70a4d916604fe78dcf6e14a902d0f9e8da0
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2021-05-06 12:15:25 -0400

    Update src/services/accounts/AccountsAssetsService.spec.ts
    Co-authored-by: Zeke Mostov <32168567+emostov@users.noreply.github.com>

--- src/services/accounts/AccountsAssetsService.spec.ts
+++ src/services/accounts/AccountsAssetsService.spec.ts
@@ -32,7 +32,7 @@ describe('AccountsAssetsService', () => {
 
 			const response = await accountsAssetsService.fetchAssetBalances(
 				blockHash789629,
-				'0xffff',
+				'0xffff', // AccountId arg here does not affect the test results
 				[10, 20]
 			);
 
