ac2a6dfbf4e6b6b0a178d22f0e3814f9df3b89db
tarikgul <tarik@parity.io>
2021-05-08 19:52:47 -0400

    docs: AssetsController

--- src/controllers/assets/AssetsController.ts
+++ src/controllers/assets/AssetsController.ts
@@ -4,15 +4,56 @@ import { RequestHandler } from 'express';
 import { AssetsService } from '../../services';
 import AbstractController from '../AbstractController';
 
+/**
+ * GET asset information for a single asset. 
+ * 
+ * Paths: 
+ * - `assetId`: The identifier of the asset.
+ * 
+ * Query:
+ * - (Optional)`at`: Block at which to retrieve runtime version information at. Block
+ *  	identifier, as the block height or block hash. Defaults to most recent block.
+ * 
+ * `/assets/:assetId/assets-info`
+ * Returns:
+ * - `at`: Block number and hash at which the call was made.
+ * - `assetsInfo`: All details concering an asset.
+ * 		- `owner`:
+ * 		- `issuer`: The `AccountId` able to mint tokens.
+ * 		- `admin`: The `AccountId` thaw tokens, force transfers and burn token from
+ * 			any account.
+ * 		- `freezer`: The `AccountId` that can freeze tokens.
+ * 		- `supply`: The total supply across accounts.
+ * 		- `deposit`: The balance deposited for this. This pays for the data stored.
+ * 		- `minBalance`: The ED for virtual accounts.
+ * 		- `isSufficient`: If `true`, then any account with this asset is given a provider reference. Otherwise, it
+ * 			requires a consumer reference.
+ * 		- `accounts`: The total number of accounts.
+ * 		- `sufficients`: The total number of accounts for which is placed a self-sufficient reference.
+ * 		- `approvals`: The total number of approvals.
+ * 		- `isFrozen`: Whether the asset is frozen for non-admin transfers.
+ * - `assetMetadata`: All metadata concerning an asset.
+ * 		- `deposit`: The balance deposited for this metadata. This pays for the data
+ * 			stored in this struct.
+ * 		- `name`: The user friendly name of this asset.
+ * 		- `symbol`: The ticker symbol for this asset.
+ * 		- `decimals`: The number of decimals this asset uses to represent one unit.
+ * 		- `isFrozen`: Whether the asset metadata may be changed by a non Force origin.
+ * 
+ * Substrate References:
+ * - Assets Pallet: https://crates.parity.io/pallet_assets/index.html
+ * - `AssetMetadata`: https://crates.parity.io/pallet_assets/struct.AssetMetadata.html
+ * - `AssetDetails`: https://crates.parity.io/pallet_assets/struct.AssetDetails.html
+ */
 export default class AssetsController extends AbstractController<AssetsService> {
 	constructor(api: ApiPromise) {
-		super(api, '/assets', new AssetsService(api));
+		super(api, '/assets/:assetId/', new AssetsService(api));
 		this.initRoutes();
 	}
 
 	protected initRoutes(): void {
 		this.safeMountAsyncGetHandlers([
-			['/:assetsId/asset-info', this.getAssetById],
+			['/asset-info', this.getAssetById],
 		]);
 	}
 
