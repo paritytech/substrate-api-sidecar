2a3b78aabc17878c057320a26a2178953319e92b
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2021-05-26 21:10:56 -0400

    Update e2e-tests/endpoints/westend/index.ts
    Co-authored-by: Andrew Plaza <aplaza@liquidthink.net>

--- e2e-tests/endpoints/westend/index.ts
+++ e2e-tests/endpoints/westend/index.ts
@@ -12,7 +12,7 @@ export const westendEndpoints = [
 	['/blocks/3032259', JSON.stringify(block3032259)], //v45
 	['/blocks/3813629', JSON.stringify(block3813629)],
 	['/blocks/3889918', JSON.stringify(block3889918)],
-	['/blocks/4781573', JSON.stringify(block4781573)],
+	['/blocks/4781573', JSON.stringify(block4781573)], //v49
 	['/blocks/5277929', JSON.stringify(block5277929)],
 	['/blocks/5480769', JSON.stringify(block5480769)], //v9000
 	['/blocks/5493461', JSON.stringify(block5493461)], //v9000
