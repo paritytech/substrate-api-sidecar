a1bc80a80d2d1fd19a7ef5f12d44354db36439ca
tarikgul <tarik@parity.io>
2021-06-02 10:15:55 -0400

    fix: update polkadot/api, lint

--- src/chains-config/westmintControllers.ts
+++ src/chains-config/westmintControllers.ts
@@ -4,25 +4,25 @@ import { ControllerConfig } from '../types/chains-config';
  * Statemint configuration for Sidecar.
  */
 export const westmintControllers: ControllerConfig = {
-    controllers: [
-        'Blocks',
-        'BlocksExtrinsics',
-        'AccountsAssets',
-        'PalletsAssets',
-        'NodeNetwork',
-        'NodeVersion',
-        'NodeTransactionPool',
-        'RuntimeCode',
-        'RuntimeSpec',
-        'RuntimeMetadata',
-        'TransactionDryRun',
-        'TransactionMaterial',
-        'TransactionFeeEstimate',
-        'TransactionSubmit',
-    ],
-    options: {
-        finalizes: true,
-        minCalcFeeRuntime: 0,
-        blockWeightStore: {},
-    },
+	controllers: [
+		'Blocks',
+		'BlocksExtrinsics',
+		'AccountsAssets',
+		'PalletsAssets',
+		'NodeNetwork',
+		'NodeVersion',
+		'NodeTransactionPool',
+		'RuntimeCode',
+		'RuntimeSpec',
+		'RuntimeMetadata',
+		'TransactionDryRun',
+		'TransactionMaterial',
+		'TransactionFeeEstimate',
+		'TransactionSubmit',
+	],
+	options: {
+		finalizes: true,
+		minCalcFeeRuntime: 0,
+		blockWeightStore: {},
+	},
 };
