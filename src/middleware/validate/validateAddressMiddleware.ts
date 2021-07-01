688d0610aa2469f099a55272b58e6fd07d177705
Crystalin <alan@purestake.com>
2021-07-01 17:20:36 +0000

    Basic support got H160 accounts

--- src/middleware/validate/validateAddressMiddleware.ts
+++ src/middleware/validate/validateAddressMiddleware.ts
@@ -22,7 +22,7 @@ export const validateAddressMiddleware: RequestHandler = (req, _res, next) => {
 };
 
 /**
- * Verify that an address is a valid substrate ss58 address.
+ * Verify that an address is a valid substrate address.
  *
  * Note: this is very similar '@polkadot/util-crypto/address/checkAddress,
  * except it does not check the prefix.
@@ -32,17 +32,28 @@ export const validateAddressMiddleware: RequestHandler = (req, _res, next) => {
 function checkAddress(address: string): [boolean, string | undefined] {
 	let decoded;
 
-	try {
-		decoded = base58Decode(address);
-	} catch (error) {
-		return [false, (error as Error).message];
+	if (address.startsWith('0x')) {
+		decoded = Uint8Array.from(Buffer.from(address.slice(2), 'hex'));
+
+		if (decoded.length != 20) {
+			return [false, `Invalid decoded address length ${decoded.length}`];
+		}
+
+		return [true, undefined];
+	} else {
+		try {
+			decoded = base58Decode(address);
+		} catch (error) {
+			return [false, (error as Error).message];
+		}
+
+		if (!defaults.allowedEncodedLengths.includes(decoded.length)) {
+			return [false, `Invalid encoded address length`];
+		}
+
+		const [isValid] = checkAddressChecksum(decoded);
+		
+		return [isValid, isValid ? undefined : 'Invalid decoded address checksum'];
 	}
-
-	if (!defaults.allowedEncodedLengths.includes(decoded.length)) {
-		return [false, 'Invalid decoded address length'];
-	}
-
-	const [isValid] = checkAddressChecksum(decoded);
-
-	return [isValid, isValid ? undefined : 'Invalid decoded address checksum'];
 }
+
