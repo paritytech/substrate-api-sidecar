ddbcb2278c4d893637ca8d03a0b9bc267a848e4b
Crystalin <alan@purestake.com>
2021-07-05 17:36:01 +0000

    lint import

--- src/middleware/validate/validateAddressMiddleware.ts
+++ src/middleware/validate/validateAddressMiddleware.ts
@@ -1,6 +1,5 @@
-import { checkAddressChecksum, isEthereumAddress } from '@polkadot/util-crypto';
-import { base58Decode } from '@polkadot/util-crypto';
 import { isHex } from '@polkadot/util';
+import { base58Decode, checkAddressChecksum, isEthereumAddress } from '@polkadot/util-crypto';
 import { defaults } from '@polkadot/util-crypto/address/defaults';
 import { RequestHandler } from 'express';
 import { BadRequest } from 'http-errors';
