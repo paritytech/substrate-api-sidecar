52124431245a58907fa7eafd594ce0c0820a2eab
tarikgul <tarik@parity.io>
2021-08-03 12:24:41 -0400

    fix: cleanup variables

--- src/chains-config/cache/lruCache.ts
+++ src/chains-config/cache/lruCache.ts
@@ -1,8 +1,8 @@
 import LRU from 'lru-cache';
 
 export const initLRUCache = (): LRU<unknown, unknown> => {
-	const config = { max: 2 },
-		cache = new LRU(config);
+	const config = { max: 2 };
+	const cache = new LRU(config);
 
 	return cache;
 };
