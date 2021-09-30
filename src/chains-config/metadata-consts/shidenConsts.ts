682312db1dcbd9d4464da2d68755d892c14d8db3
Hoon Kim <40356749+hoonsubin@users.noreply.github.com>
2021-09-30 12:46:30 +0200

    add base weight and runtime version

--- src/chains-config/metadata-consts/shidenConsts.ts
+++ src/chains-config/metadata-consts/shidenConsts.ts
@@ -1,8 +1,11 @@
 import { MetadataConsts } from '../../types/chains-config';
+import { extrinsicBaseWeight } from './substrateConsts';
 
 export const shidenDefinitions: MetadataConsts[] = [
 	{
-		runtimeVersions: [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022],
-		extrinsicBaseWeight: BigInt(65000000),
+		runtimeVersions: [
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+		],
+		extrinsicBaseWeight,
 	},
 ];
