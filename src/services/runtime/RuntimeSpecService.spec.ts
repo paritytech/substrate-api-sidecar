e08210191804f85e357f8b7922fc7e1066726d7d
tarikgul <tarik@parity.io>
2021-10-08 17:38:22 -0400

    refactor RuntimeSpecService.spec.ts

--- src/services/runtime/RuntimeSpecService.spec.ts
+++ src/services/runtime/RuntimeSpecService.spec.ts
@@ -1,9 +1,9 @@
 import { sanitizeNumbers } from '../../sanitize/sanitizeNumbers';
-import { blockHash789629, mockApi } from '../test-helpers/mock';
+import { blockHash789629, defaultMockApi } from '../test-helpers/mock';
 import response from '../test-helpers/responses/runtime/spec.json';
 import { RuntimeSpecService } from './RuntimeSpecService';
 
-const runtimeSpecService = new RuntimeSpecService(mockApi);
+const runtimeSpecService = new RuntimeSpecService(defaultMockApi);
 
 describe('RuntimeSpecService', () => {
 	describe('fetchSpec', () => {
