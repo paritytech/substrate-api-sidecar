a26cc75b490cbeb108afb54a4ca60b5c6df60f04
tarikgul <tarik@parity.io>
2021-10-14 15:20:59 -0400

    fix: vesting-info tests, add another test as well

--- src/services/accounts/AccountsVestingInfoService.spec.ts
+++ src/services/accounts/AccountsVestingInfoService.spec.ts
@@ -1,8 +1,10 @@
 import { ApiPromise } from '@polkadot/api';
 import { Hash } from '@polkadot/types/interfaces';
 
+import { createApiWithAugmentations, TypeFactory } from '../../test-helpers/typeFactory';
 import { sanitizeNumbers } from '../../sanitize/sanitizeNumbers';
 import { polkadotRegistry } from '../../test-helpers/registries';
+import { polkadotMetadataRpcV9110 } from '../../test-helpers/metadata/polkadotV9110Metadata';
 import {
 	blockHash789629,
 	defaultMockApi,
@@ -11,10 +13,21 @@ import {
 import response789629 from '../test-helpers/responses/accounts/vestingInfo789629.json';
 import { AccountsVestingInfoService } from './AccountsVestingInfoService';
 
+const typeFactorApiV9110 = createApiWithAugmentations(polkadotMetadataRpcV9110);
+const factory = new TypeFactory(typeFactorApiV9110);
+
 const vestingAt = (_hash: Hash, _address: string) =>
-	Promise.resolve().then(() =>
-		polkadotRegistry.createType('Option<VestingInfo>', null)
-	);
+	Promise.resolve().then(() => {
+		const vestingRes = {
+			"locked": "1749990000000000",
+			"perBlock": "166475460",
+			"startingBlock": "4961000"
+		}
+		const vestingInfo = typeFactorApiV9110.createType('PalletVestingVestingInfo', vestingRes);
+		const vecVestingInfo = factory.vecOf([vestingInfo]);
+
+		return factory.optionOf(vecVestingInfo);
+	});
 
 const mockApi = {
 	...defaultMockApi,
@@ -29,7 +42,7 @@ const accountsVestingInfoService = new AccountsVestingInfoService(mockApi);
 
 describe('AccountVestingInfoService', () => {
 	describe('fetchAccountVestingInfo', () => {
-		it('works when ApiPromise works (block 789629)', async () => {
+		it('works when ApiPromise works (block 789629) with V14 metadata', async () => {
 			expect(
 				sanitizeNumbers(
 					await accountsVestingInfoService.fetchAccountVestingInfo(
@@ -38,6 +51,29 @@ describe('AccountVestingInfoService', () => {
 					)
 				)
 			).toStrictEqual(response789629);
+		});
+
+		it('Vesting should return an empty array for None responses', async () => {
+			const tempVest = () => Promise.resolve().then(() => polkadotRegistry.createType('Option<VestingInfo>', null));
+			(mockApi.query.vesting.vesting.at as unknown) = tempVest;
+
+			const expectedResponse = {
+				at: {
+					hash: "0x7b713de604a99857f6c25eacc115a4f28d2611a23d9ddff99ab0e4f1c17a8578",
+					height: "789629"
+				},
+				vesting: []
+			}
+
+			expect(
+				sanitizeNumbers(
+					await accountsVestingInfoService.fetchAccountVestingInfo(
+						blockHash789629,
+						testAddress
+					)
+				)
+			).toStrictEqual(expectedResponse);
+			(mockApi.query.vesting.vesting.at as unknown) = vestingAt
 		});
 	});
 });
