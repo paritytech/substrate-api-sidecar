91056a8c43e35d56a73711ba8a90e301dd6066d1
tarikgul <tarik@parity.io>
2021-10-18 22:25:42 -0400

    broken tests

---
+++ src/middleware/connection/reconnectMiddleware.spec.ts
@@ -0,0 +1,25 @@
+import { ApiPromise } from '@polkadot/api';
+
+import { Request, Response } from 'express';
+import { reconnectMiddleware } from './reconnectMiddleware';
+
+
+const mockApi  = {
+
+} as unknown as ApiPromise
+
+describe('reconnectMiddleware', () => {
+    const apiConnectionCache = {
+        isReconnecting: false
+    }
+
+    it('Should successfully call next when connected', async () => {
+        const res = jest.fn() as unknown as Response,
+              req = jest.fn() as unknown as Request,
+              next = jest.fn();
+
+        reconnectMiddleware(mockApi, apiConnectionCache)(req, res, next);
+
+        expect(next).toBeCalled();
+    });
+});
