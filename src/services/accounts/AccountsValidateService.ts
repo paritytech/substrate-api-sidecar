9d3dab628d85565454519bfb1aa26115e7eeed27
tarikgul <tarik@parity.io>
2021-10-25 08:45:11 -0400

    remove networkId

--- src/services/accounts/AccountsValidateService.ts
+++ src/services/accounts/AccountsValidateService.ts
@@ -1,4 +1,3 @@
-import { knownSubstrate } from '@polkadot/networks/substrate';
 import { hexToU8a, isHex } from '@polkadot/util';
 import { base58Decode, checkAddressChecksum } from '@polkadot/util-crypto';
 import { defaults } from '@polkadot/util-crypto/address/defaults';
@@ -7,8 +6,7 @@ import { AbstractService } from '../AbstractService';
 
 interface ValidateAddrResponse {
 	isValid: boolean;
-	networkId: string | null;
-	ss58: number | null;
+	ss58Prefix: number | null;
 }
 
 export class AccountsValidateService extends AbstractService {
@@ -22,30 +20,23 @@ export class AccountsValidateService extends AbstractService {
 			} catch {
 				return {
 					isValid: false,
-					networkId: null,
-					ss58: null,
+					ss58Prefix: null,
 				};
 			}
 		}
 
 		if (defaults.allowedEncodedLengths.includes(u8Address.length)) {
 			const [isValid, , , ss58Decoded] = checkAddressChecksum(u8Address);
-			const isValidNetworkId = this.api.registry.chainSS58 === ss58Decoded;
-			const networkInfo = knownSubstrate.filter(
-				(obj) => obj.prefix === ss58Decoded
-			)[0];
 
 			return {
-				isValid: isValid && isValidNetworkId,
-				networkId: networkInfo ? networkInfo.network : null,
-				ss58: ss58Decoded,
+				isValid: isValid,
+				ss58Prefix: ss58Decoded,
 			};
 		}
 
 		return {
 			isValid: false,
-			networkId: null,
-			ss58: null,
+			ss58Prefix: null,
 		};
 	}
 }
