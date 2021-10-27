3de3b46a1f74289cdc928740c85a4652448807c6
tarikgul <tarik@parity.io>
2021-10-26 23:05:34 -0400

    cleanup testws

--- src/services/accounts/AccountsValidateService.spec.ts
+++ src/services/accounts/AccountsValidateService.spec.ts
@@ -3,19 +3,10 @@ import { ApiPromise } from '@polkadot/api';
 import { sanitizeNumbers } from '../../sanitize';
 import { AccountsValidateService } from './AccountsValidateService';
 
+const mockApi = {} as unknown as ApiPromise;
+const validateService = new AccountsValidateService(mockApi);
+
 describe('Validate addresses', () => {
-	const mockApi = {
-		registry: {
-			chainSS58: 0,
-		},
-	} as unknown as ApiPromise;
-	const validateService = new AccountsValidateService(mockApi);
-
-	afterEach(() => {
-		// Reset the `mockApi.registry.chainSS58` to be 0  before each test
-		(mockApi.registry.chainSS58 as unknown) = 0;
-	});
-
 	it('Should verify a polkadot address when connected to polkadot', () => {
 		const expectedResponse = {
 			isValid: true,
@@ -34,7 +25,6 @@ describe('Validate addresses', () => {
 			ss58Prefix: '2',
 		};
 		const kusamaAddr = 'DXgXPAT5zWtPHo6FhVvrDdiaDPgCNGxhJAeVBYLtiwW9hAc';
-		(mockApi.registry.chainSS58 as unknown) = 2;
 
 		expect(
 			sanitizeNumbers(validateService.validateAddress(kusamaAddr))
@@ -47,7 +37,6 @@ describe('Validate addresses', () => {
 			ss58Prefix: '16',
 		};
 		const kulupuAddr = '2cYv9Gk6U4m4a7Taw9pG8qMfd1Pnxw6FLTvV6kYZNhGL6M9y';
-		(mockApi.registry.chainSS58 as unknown) = 16;
 
 		expect(
 			sanitizeNumbers(validateService.validateAddress(kulupuAddr))
@@ -60,23 +49,9 @@ describe('Validate addresses', () => {
 			ss58Prefix: '42',
 		};
 		const substrateAddr = '5EnxxUmEbw8DkENKiYuZ1DwQuMoB2UWEQJZZXrTsxoz7SpgG';
-		(mockApi.registry.chainSS58 as unknown) = 42;
 
 		expect(
 			sanitizeNumbers(validateService.validateAddress(substrateAddr))
-		).toStrictEqual(expectedResponse);
-	});
-
-	it('Should correctly validate a polkadot address on kusama', () => {
-		const expectedResponse = {
-			isValid: true,
-			ss58Prefix: '0',
-		};
-		const polkadotAddr = '1xN1Q5eKQmS5AzASdjt6R6sHF76611vKR4PFpFjy1kXau4m';
-		(mockApi.registry.chainSS58 as unknown) = 2;
-
-		expect(
-			sanitizeNumbers(validateService.validateAddress(polkadotAddr))
 		).toStrictEqual(expectedResponse);
 	});
 
@@ -100,7 +75,6 @@ describe('Validate addresses', () => {
 		};
 		const kusamaHex =
 			'0x02ce046d43fc4c0fb8b3b754028515e5020f5f1d8d620b4ef0f983c5df34b1952909e9';
-		(mockApi.registry.chainSS58 as unknown) = 2;
 
 		expect(
 			sanitizeNumbers(validateService.validateAddress(kusamaHex))
@@ -114,7 +88,6 @@ describe('Validate addresses', () => {
 		};
 		const karuraHex =
 			'0x086d6f646c6163612f6364707400000000000000000000000000000000000000008333';
-		(mockApi.registry.chainSS58 as unknown) = 8;
 
 		expect(
 			sanitizeNumbers(validateService.validateAddress(karuraHex))
