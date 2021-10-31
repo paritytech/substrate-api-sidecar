2559ec7c2a9d7e7e83def1b6afc8a9afac9574f9
tarikgul <tarik@parity.io>
2021-10-31 13:02:14 -0400

    e2e tests for staking info

--- e2e-tests/endpoints/polkadot/accounts/index.ts
+++ e2e-tests/endpoints/polkadot/accounts/index.ts
@@ -1,9 +1,11 @@
 import { polkadotAccountBalanceEndpoints } from './balance-info';
 import { polkadotAccountValidateEndpoints } from './validate';
 import { polkadotAccountVestingInfoEndpoints } from './vesting-info';
+import { polkadotAccountStakingInfoEndpoints } from './staking-info';
 
 export const polkadotAccountsEndpoints = [
 	...polkadotAccountBalanceEndpoints,
+	...polkadotAccountStakingInfoEndpoints,
 	...polkadotAccountVestingInfoEndpoints,
 	...polkadotAccountValidateEndpoints,
 ];
