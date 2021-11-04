5c9757090489c3313c7348cdb35e816b27c36fbd
Rob Thijssen <rthijssen@gmail.com>
2021-11-04 16:41:34 +0200

    whitespace for the linter

--- src/chains-config/metadata-consts/calamariConsts.ts
+++ src/chains-config/metadata-consts/calamariConsts.ts
@@ -2,8 +2,8 @@ import { MetadataConsts } from '../../types/chains-config';
 import { extrinsicBaseWeight } from './substrateConsts';
 
 export const calamariDefinitions: MetadataConsts[] = [
-  {
-    runtimeVersions: [1, 2, 3, 4],
-    extrinsicBaseWeight,
-  },
+	{
+		runtimeVersions: [1, 2, 3, 4],
+		extrinsicBaseWeight,
+	},
 ];
