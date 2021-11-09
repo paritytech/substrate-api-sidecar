273cac2ae7e9d7ff05a5a4146d4ce1ee5090e773
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2021-11-09 09:47:46 -0500

    feat: add query param `metadata` (#746)
    * add query param decodeMeta
    
    * update docs
    
    * fix test description
    
    * lint
    
    * fix up the query param to have both noMeta and metadata without introducing breaking changes
    
    * update logic, and clean it up
    
    * lint
    
    * change ifs to switch
    
    * update the docs
    
    * update more docs
    
    * build:docs
    
    * inline comment

--- src/types/responses/TransactionMaterial.ts
+++ src/types/responses/TransactionMaterial.ts
@@ -1,5 +1,6 @@
 import { BlockHash } from '@polkadot/types/interfaces';
 import { u32 } from '@polkadot/types/primitive';
+import { AnyJson } from '@polkadot/types/types';
 
 import { IAt } from '.';
 
@@ -10,5 +11,5 @@ export interface ITransactionMaterial {
 	specName: string;
 	specVersion: u32;
 	txVersion: u32;
-	metadata?: string;
+	metadata?: string | Record<string, AnyJson>;
 }
