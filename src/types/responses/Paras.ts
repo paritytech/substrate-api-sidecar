ce2ff0bfb1fb21207c0da92b2a7d92b5f262f5f3
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2021-11-10 11:31:35 -0500

    fix(ParasService): adjust endpoint to use historicApi, fix endingOffset bug (#735)
    * add historicApi to crowdloans-info
    
    * add assertQueryModule check
    
    * add historicApi to crowdloans
    
    * add historicApi to leases-info and leases/current
    
    * add historicApi to auctions-current
    
    * add historicApi to paras
    
    * fix endingOffset bug
    
    * adjust tests to reflect historicApi
    
    * cleanup
    
    * inline docs
    
    * lint
    
    * Update src/services/paras/ParasService.ts
    
    Co-authored-by: Zeke Mostov <32168567+emostov@users.noreply.github.com>
    
    * adjust lease_period_index
    
    * lease period index edgecase
    
    * adjust leasePeriodIndex to be null not BN_ZERO
    
    * update docs
    
    Co-authored-by: Zeke Mostov <32168567+emostov@users.noreply.github.com>

--- src/types/responses/Paras.ts
+++ src/types/responses/Paras.ts
@@ -50,9 +50,10 @@ export interface ICrowdloans {
 
 export interface LeaseFormatted {
 	/**
-	 * Lease period.
+	 * Lease period. The `leasePeriodIndex` can be null when the current block now,
+	 * substracted by the offset is less than zero.
 	 */
-	leasePeriodIndex: number;
+	leasePeriodIndex: number | null;
 	/**
 	 * Amount held on deposit for the lease period.
 	 */
@@ -132,13 +133,15 @@ export interface IAuctionsCurrent {
 export interface ILeasesCurrent {
 	at: IAt;
 	/**
-	 * The current lease period.
+	 * The current lease period. The `leasePeriodIndex` can be null when the current block now,
+	 * substracted by the offset is less than zero.
 	 */
-	leasePeriodIndex: BN;
+	leasePeriodIndex: BN | null;
 	/**
-	 * Last block of the current lease period.
+	 * Last block of the current lease period. The `endOfLeasePeriod` may be null
+	 * when the `leasePeriodIndex` is null.
 	 */
-	endOfLeasePeriod: BN;
+	endOfLeasePeriod: BN | null;
 	/**
 	 * ParaIds of current lease holders.
 	 */
