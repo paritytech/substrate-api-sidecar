dd6a43a25d223eed3d8bc0880838ef2f19119aaa
tarikgul <tarik@parity.io>
2021-11-15 09:05:46 -0500

    fix: remove /experimental from e2e tests

--- e2e-tests/endpoints/kusama/paras/auctions-current/index.ts
+++ e2e-tests/endpoints/kusama/paras/auctions-current/index.ts
@@ -3,11 +3,11 @@ import parasAuctionsCurrent9828507 from './9828507.json';
 
 export const parasAuctionsCurrentEndpoints = [
 	[
-		'/experimental/paras/auctions/current?at=8400000',
+		'/paras/auctions/current?at=8400000',
 		JSON.stringify(parasAuctionsCurrent8400000),
 	],
 	[
-		'/experimental/paras/auctions/current?at=9828507',
+		'/paras/auctions/current?at=9828507',
 		JSON.stringify(parasAuctionsCurrent9828507),
 	],
 ];
