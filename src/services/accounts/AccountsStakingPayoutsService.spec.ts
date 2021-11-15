ff2f2223c4e2c4d03f5626693f8b092e63146136
tarikgul <tarik@parity.io>
2021-11-15 10:40:46 -0500

    boilerplate for tests

--- src/services/accounts/AccountsStakingPayoutsService.spec.ts
+++ src/services/accounts/AccountsStakingPayoutsService.spec.ts
@@ -1,14 +1,60 @@
 import { ApiPromise } from '@polkadot/api';
 import { ApiDecoration } from '@polkadot/api/types';
-import { Hash } from '@polkadot/types/interfaces';
+import { u32 } from '@polkadot/types';
+import { Hash, EraIndex, AccountId32 } from '@polkadot/types/interfaces';
 
 import { defaultMockApi } from '../test-helpers/mock';
 import { AccountsStakingPayoutsService } from './AccountsStakingPayoutsService';
 
-const mockHistoricApi = {} as unknown as ApiDecoration<'promise'>;
+const historyDepthAt = () => 
+	Promise.resolve().then(() => {
+
+	});
+	
+const erasRewardPointsAt = (_eraIndex: EraIndex) =>
+	Promise.resolve().then(() => {
+
+	});
+
+const erasValidatorRewardAt = (_eraIndex: EraIndex) =>
+	Promise.resolve().then(() => {
+
+	});
+
+const erasValidatorPrefsAt = (_era: u32 | number, _validatorId: string | AccountId32) => 
+	Promise.resolve().then(() => {
+
+	});
+
+const bondedAt = (_validatorId: string | AccountId32) =>
+	Promise.resolve().then(() => {
+
+	});
+
+const deriveEraExposure = (_eraIndex: EraIndex) => 
+	Promise.resolve().then(() => {
+
+	});
+
+const mockHistoricApi = {
+	query: {
+		staking: {
+			erasRewardPoints: erasRewardPointsAt,
+			erasValidatorReward: erasValidatorRewardAt,
+			historyDepth: historyDepthAt,
+			erasValidatorPrefs: erasValidatorPrefsAt,
+			bonded: bondedAt,
+		}
+	},
+} as unknown as ApiDecoration<'promise'>;
 
 const mockApi = {
 	...defaultMockApi,
+	derive: {
+		staking: {
+			eraExposure: deriveEraExposure,
+		},
+	},
 	at: (_hash: Hash) => mockHistoricApi,
 } as unknown as ApiPromise;
 
