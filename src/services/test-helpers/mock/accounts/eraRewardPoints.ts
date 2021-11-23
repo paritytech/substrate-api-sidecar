c0de98a227500538b566f45cd03a51b67c2b67fb
tarikgul <tarik@parity.io>
2021-11-22 22:47:22 -0500

    mapToBe -> mockEraRewardsMap

--- src/services/test-helpers/mock/accounts/eraRewardPoints.ts
+++ src/services/test-helpers/mock/accounts/eraRewardPoints.ts
@@ -4,14 +4,14 @@ import { polkadotRegistryV9110 } from '../../../../test-helpers/registries';
 
 const rewardOne = polkadotRegistryV9110.createType('u32', 3360);
 const rewardTwo = polkadotRegistryV9110.createType('u32', 4440);
-const mapToBe = new Map();
-mapToBe.set('12JZr1HgK8w6zsbBj6oAEVRkvisn8j3MrkXugqtvc4E8uwLo', rewardOne);
-mapToBe.set('1HDgY7vpDjafR5NM8dbwm1b3Rrs4zATuSCHHbe7YgpKUKFw', rewardTwo);
+const mockEraRewardsMap = new Map();
+mockEraRewardsMap.set('12JZr1HgK8w6zsbBj6oAEVRkvisn8j3MrkXugqtvc4E8uwLo', rewardOne);
+mockEraRewardsMap.set('1HDgY7vpDjafR5NM8dbwm1b3Rrs4zATuSCHHbe7YgpKUKFw', rewardTwo);
 const bTree = new BTreeMap(
 	polkadotRegistryV9110,
 	'AccountId32',
 	'u32',
-	mapToBe
+	mockEraRewardsMap
 );
 
 /** Used for StakingPayouts */
