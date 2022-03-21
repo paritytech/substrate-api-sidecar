a63237a2eb17d20b42388efbe057f8434f5d15f3
tarikgul <tarik@parity.io>
2022-03-20 21:38:32 -0400

    update e2e tests for statemint

--- e2e-tests/endpoints/statemint/blocks/index.ts
+++ e2e-tests/endpoints/statemint/blocks/index.ts
@@ -2,6 +2,6 @@ import block10000 from './10000.json';
 import block340000 from './340000.json';
 
 export const statemintBlockEndpoints = [
-	['/blocks/10000', JSON.stringify(block10000)],
-	['/blocks/340000', JSON.stringify(block340000)],
+	['/blocks/10000', JSON.stringify(block10000)], // v2
+	['/blocks/340000', JSON.stringify(block340000)], // v601
 ];
