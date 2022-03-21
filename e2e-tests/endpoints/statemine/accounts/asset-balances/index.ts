75844d6f4f4e6a3d7dcb94f67f81aac77591f8d8
tarikgul <tarik@parity.io>
2022-03-20 21:16:06 -0400

    update statemine with e2e-tests

--- e2e-tests/endpoints/statemine/accounts/asset-balances/index.ts
+++ e2e-tests/endpoints/statemine/accounts/asset-balances/index.ts
@@ -2,6 +2,9 @@ import accountAssetBalance450000 from './450000.json';
 import accountAssetBalance650000 from './650000.json';
 import accountAssetBalance960000 from './960000.json';
 import accountAssetBalance986791 from './986791.json';
+import accountAssetBalance1100000 from './1100000.json';
+import accountAssetBalance1300000 from './1300000.json';
+import accountAssetBalance1800000 from './1800000.json';
 
 export const statemineAccountAssetBalanceEndpoints = [
 	[
@@ -20,4 +23,16 @@ export const statemineAccountAssetBalanceEndpoints = [
 		'/accounts/H4DU1hKQeLkR5bhMeMidarF9bVvrH3k6ybLz84YLs7eRQMu/asset-balances?assets[]=11&at=986791',
 		JSON.stringify(accountAssetBalance986791),
 	], // v5
+	[
+		'/accounts/H4DU1hKQeLkR5bhMeMidarF9bVvrH3k6ybLz84YLs7eRQMu/asset-balances?assets[]=11&at=1100000',
+		JSON.stringify(accountAssetBalance1100000),
+	], // v504
+	[
+		'/accounts/H4DU1hKQeLkR5bhMeMidarF9bVvrH3k6ybLz84YLs7eRQMu/asset-balances?assets[]=11&at=1300000',
+		JSON.stringify(accountAssetBalance1300000),
+	], // v601
+	[
+		'/accounts/H4DU1hKQeLkR5bhMeMidarF9bVvrH3k6ybLz84YLs7eRQMu/asset-balances?assets[]=11&at=1800000',
+		JSON.stringify(accountAssetBalance1800000),
+	], // v700
 ];
--- e2e-tests/endpoints/statemine/blocks/index.ts
+++ e2e-tests/endpoints/statemine/blocks/index.ts
@@ -1,4 +1,17 @@
+import block450000 from './450000.json';
+import block650000 from './650000.json';
+import block960000 from './960000.json';
 import block993540 from './993450.json';
+import block1100000 from './1100000.json';
+import block1300000 from './1300000.json';
+import block1800000 from './1800000.json';
+
 export const statemineBlockEndpoints = [
-	['/blocks/993540', JSON.stringify(block993540)],
+	['/blocks/450000', JSON.stringify(block450000)], // v2
+	['/blocks/650000', JSON.stringify(block650000)], // v3
+	['/blocks/960000', JSON.stringify(block960000)], // v4
+	['/blocks/993540', JSON.stringify(block993540)], // v5
+	['/blocks/1100000', JSON.stringify(block1100000)], // v504
+	['/blocks/1300000', JSON.stringify(block1300000)], // v601
+	['/blocks/1800000', JSON.stringify(block1800000)], // v700
 ];
