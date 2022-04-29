f25ce800e9d85202c279e8fb15179221c7fb8eb3
tarikgul <tarik@parity.io>
2022-04-29 17:15:38 -0400

    test(e2e): add V14 metadata e2e tests

--- e2e-tests/endpoints/polkadot/runtime/metadata/index.ts
+++ e2e-tests/endpoints/polkadot/runtime/metadata/index.ts
@@ -1,7 +1,9 @@
 import runtimeMetadata100000 from './100000.json';
 import runtimeMetadata6877002 from './6877002.json';
+import runtimeMetadata10090000 from './10090000.json';
 
 export const polkadotRuntimeMetadataEndpoints = [
 	['/runtime/metadata?at=100000', JSON.stringify(runtimeMetadata100000)], // v1
-	['/runtime/metadata?at=6877002', JSON.stringify(runtimeMetadata6877002)], // v9090
+	['/runtime/metadata?at=6877002', JSON.stringify(runtimeMetadata6877002)], // v9090 V13
+	['/runtime/metadata?at=10090000', JSON.stringify(runtimeMetadata10090000)], // v9180 V14
 ];
--- e2e-tests/endpoints/westend/runtime/metadata/index.ts
+++ e2e-tests/endpoints/westend/runtime/metadata/index.ts
@@ -1,7 +1,9 @@
 import runtimeMetadata10000 from './10000.json';
 import runtimeMetadata7417678 from './7417678.json';
+import runtimeMetadata10620000 from './10620000.json';
 
 export const westendRuntimeMetadataEndpoints = [
 	['/runtime/metadata?at=10000', JSON.stringify(runtimeMetadata10000)], // v1
-	['/runtime/metadata?at=7417678', JSON.stringify(runtimeMetadata7417678)], // v9090
+	['/runtime/metadata?at=7417678', JSON.stringify(runtimeMetadata7417678)], // v9090 V13
+	['/runtime/metadata?at=10620000', JSON.stringify(runtimeMetadata10620000)], // v9200 V14
 ];
