0a6770c7e8e05e82c3b71fc12395a9b5d8e4f367
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2022-05-09 10:37:47 -0400

    test(e2e): add V14 metadata e2e tests (#909)

--- e2e-tests/endpoints/kusama/runtime/metadata/index.ts
+++ e2e-tests/endpoints/kusama/runtime/metadata/index.ts
@@ -1,7 +1,9 @@
 import runtimeMetadata10000 from './10000.json';
 import runtimeMetadata8000000 from './8000000.json';
+import runtimeMetadata12470000 from './12470000.json';
 
 export const kusamaRuntimeMetadataEndpoints = [
 	['/runtime/metadata?at=10000', JSON.stringify(runtimeMetadata10000)], // v1020
-	['/runtime/metadata?at=8000000', JSON.stringify(runtimeMetadata8000000)], // v9040
+	['/runtime/metadata?at=8000000', JSON.stringify(runtimeMetadata8000000)], // v9040 V13
+	['/runtime/metadata?at=12470000', JSON.stringify(runtimeMetadata12470000)], // v9190 V14
 ];
