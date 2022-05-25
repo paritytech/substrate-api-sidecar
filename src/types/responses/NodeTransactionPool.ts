5a055d0704f23832dba73c3e47b4dd146fe026a6
tarikgul <tarik@parity.io>
2022-05-25 15:22:34 -0400

    update the return type to include tip

--- src/types/responses/NodeTransactionPool.ts
+++ src/types/responses/NodeTransactionPool.ts
@@ -1,6 +1,17 @@
 interface IPoolExtrinsic {
+	/**
+	 * H256 hash of the extrinsic
+	 */
 	hash: string;
+	/**
+	 * SCALE encoded extrinsic
+	 */
 	encodedExtrinsic: string;
+	/**
+	 * The tip within an extrinsic. Available when the `tip` query parameter
+	 * for `/node/transaction-pool` is set to true.
+	 */
+	tip?: string;
 }
 
 export interface INodeTransactionPool {
