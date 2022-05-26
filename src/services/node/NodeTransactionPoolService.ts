ad2fbaaff2c3573b068110d1985a2ed4b7e8dd31
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2022-05-26 16:14:12 -0400

    fix: NodeTransactionPoolService response import  (#929)
    * fix: small import tech debt
    
    * lint

--- src/services/node/NodeTransactionPoolService.ts
+++ src/services/node/NodeTransactionPoolService.ts
@@ -14,8 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { INodeTransactionPool } from 'src/types/responses';
-
+import { INodeTransactionPool } from '../../types/responses';
 import { AbstractService } from '../AbstractService';
 
 export class NodeTransactionPoolService extends AbstractService {
