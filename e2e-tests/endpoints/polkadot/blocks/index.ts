8906816934b8e6bd5bf98393f83d55a6711d296e
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2022-05-26 14:42:56 -0400

    chore(license): append the license to the top of each file (#927)
    * chore(license): append the license to the top of each file
    
    * lint
    
    * add licensing to calc

--- e2e-tests/endpoints/polkadot/blocks/index.ts
+++ e2e-tests/endpoints/polkadot/blocks/index.ts
@@ -1,3 +1,19 @@
+// Copyright 2017-2022 Parity Technologies (UK) Ltd.
+// This file is part of Substrate API Sidecar.
+//
+// Substrate API Sidecar is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import block943438 from './943438.json';
 import block1603025 from './1603025.json';
 import block1662525 from './1662525.json';
--- src/services/test-helpers/mock/data/localListenAddresses.ts
+++ src/services/test-helpers/mock/data/localListenAddresses.ts
@@ -1,2 +1,18 @@
+// Copyright 2017-2022 Parity Technologies (UK) Ltd.
+// This file is part of Substrate API Sidecar.
+//
+// Substrate API Sidecar is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 export const localListenAddressesHex =
 	'0x1045012f6970342f3132372e302e302e312f7463702f33303333332f7032702f313244334b6f6f57415a66686a79717a4674796435357665424a78545969516b5872614d584c704d4d6a355a6f3471756431485a49012f6970342f31302e3230312e302e322f7463702f33303333332f7032702f313244334b6f6f57415a66686a79717a4674796435357665424a78545969516b5872614d584c704d4d6a355a6f3471756431485a45012f6970342f31302e312e312e34332f7463702f33303333332f7032702f313244334b6f6f57415a66686a79717a4674796435357665424a78545969516b5872614d584c704d4d6a355a6f3471756431485a2d012f6970362f3a3a312f7463702f33303333332f7032702f313244334b6f6f57415a66686a79717a4674796435357665424a78545969516b5872614d584c704d4d6a355a6f3471756431485a';
