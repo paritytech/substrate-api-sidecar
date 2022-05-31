beeb5e8ea7e7d3789fdfafb77f9aeeb6d729ae52
tarikgul <tarik@parity.io>
2022-05-31 12:07:10 -0400

    update response type for partialfee

--- src/types/responses/NodeTransactionPool.ts
+++ src/types/responses/NodeTransactionPool.ts
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import { Balance } from '@polkadot/types/interfaces';
+
 interface IPoolExtrinsic {
 	/**
 	 * H256 hash of the extrinsic
@@ -31,7 +33,7 @@ interface IPoolExtrinsic {
 	/**
 	 * PartialFee for a transaction
 	 */
-	partialFee?: string;
+	partialFee?: Balance;
 	/**
 	 * Priority of the transaction. Calculated by tip / {Weight | Length}
 	 */
