715b0de592830d033e0255a41d191cfc074586db
tarikgul <tarik@parity.io>
2022-06-24 17:59:31 -0400

    fix compare.spec.ts

--- src/util/integers/compare.spec.ts
+++ src/util/integers/compare.spec.ts
@@ -4,10 +4,12 @@ import { subIntegers } from './compare';
 
 describe('Compare integers', () => {
 	describe('subIntegers', () => {
-		const foo = new BN(100);
-		const bar = new BN(101);
+		it('Should return the correct value for any ordered input', () => {
+			const foo = new BN(100);
+			const bar = new BN(101);
 
-		expect(subIntegers(foo, bar)).toBe(1);
-		expect(subIntegers(bar, foo)).toBe(1);
+			expect(subIntegers(foo, bar).toString()).toBe('1');
+			expect(subIntegers(bar, foo).toString()).toBe('1');
+		});
 	});
 });
