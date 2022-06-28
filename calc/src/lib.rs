6b0afe8adb80e7b2baea5836b47d124283a01731
tarikgul <tarik@parity.io>
2022-06-28 18:01:49 -0400

    export mod

--- calc/src/lib.rs
+++ calc/src/lib.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub mod calc_actual_fee;
 pub mod calc_fee;
 pub mod calc_payout;
 mod debug;
