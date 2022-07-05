ef513cc7f26773df776b9fe77cef8046bcadcea6
Dominique <dsaripapazoglou@gmail.com>
2022-07-04 21:00:40 -0400

    feat: Added the `network` and `accountId` to the response of `validate` endpoint (#926)
    * Added networkName in the response of validate address endpoint
    
    * Added the 'accountId' in the endpoint response
    - If the address given is not a hex value then it returns the hex value of the related registry type
    - If the address given is a hex value then it returns the same as the 'accountId'
    - The tests were updated based on this logic
    
    * Changes in `validateAddress` function, tests & docs
    - Replaced `filter` with a `for..of` loop when retrieving the `network`.
    - Replaced `new TypeRegistry` with `this.api.registry` when retrieving the `accountID`.
    - Renamed `networkName` to `network` so it is aligned with the key naming in the SS58 registry.
    - Updated the JSDocs of the function `validateAddress`.
    - Added the default mock api into the service in the tests.
    - Updated the docs.
    
    * Code changes & built docs
    - Changes in code based on Tarik's feedback.
    - Run `yarn build:docs`.
    
    * Removed unnecessary variable
    
    * Fix for accountId & corresponding tests
    - Fix for accountId when the input from user is an address in hex (from u8 array) format.
    - Corrected the output of the corresponding tests also.
    
    * Returns  in all other fields if isValid is false
    
    * Code changes based on James feedback
    
    * yarn lint

--- src/services/accounts/AccountsValidateService.ts
+++ src/services/accounts/AccountsValidateService.ts
@@ -15,7 +15,11 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { hexToU8a, isHex } from '@polkadot/util';
-import { base58Decode, checkAddressChecksum } from '@polkadot/util-crypto';
+import {
+	allNetworks,
+	base58Decode,
+	checkAddressChecksum,
+} from '@polkadot/util-crypto';
 import { defaults } from '@polkadot/util-crypto/address/defaults';
 
 import { IValidateAddrResponse } from '../../types/responses/ValidateAddress';
@@ -23,14 +27,18 @@ import { AbstractService } from '../AbstractService';
 
 export class AccountsValidateService extends AbstractService {
 	/**
-	 * Takes a given address and determines whether it is a ss58 formatted address,
-	 * and what the ss58 prefix for that address is.
+	 * Takes a given address and determines whether it is a ss58 or
+	 * a hex (from a u8 array) formatted address,
+	 * what the ss58 prefix for that address is,
+	 * what is the network address format,
+	 * and what the account ID is for this address.
 	 *
 	 * @param address ss58 or hex address to validate
 	 */
 	validateAddress(address: string): IValidateAddrResponse {
 		let u8Address;
-		if (isHex(address)) {
+		const addressIsHex = isHex(address);
+		if (addressIsHex) {
 			u8Address = hexToU8a(address);
 		} else {
 			try {
@@ -42,16 +50,31 @@ export class AccountsValidateService extends AbstractService {
 
 		if (defaults.allowedEncodedLengths.includes(u8Address.length)) {
 			const [isValid, , , ss58Prefix] = checkAddressChecksum(u8Address);
-
-			return {
-				isValid: isValid,
-				ss58Prefix,
-			};
+			if (isValid) {
+				let network = null;
+				for (const networkParams of allNetworks) {
+					if (networkParams['prefix'] === ss58Prefix) {
+						network = networkParams['network'];
+						break;
+					}
+				}
+				const accountId = addressIsHex
+					? '0x' + address.slice(4, -4)
+					: this.api.registry.createType('AccountId', address).toHex();
+				return {
+					isValid: isValid,
+					ss58Prefix,
+					network,
+					accountId,
+				};
+			}
 		}
 
 		return {
 			isValid: false,
 			ss58Prefix: null,
+			network: null,
+			accountId: null,
 		};
 	}
 }
--- src/types/responses/ValidateAddress.ts
+++ src/types/responses/ValidateAddress.ts
@@ -17,4 +17,6 @@
 export interface IValidateAddrResponse {
 	isValid: boolean;
 	ss58Prefix: number | null;
+	network: string | null;
+	accountId: string | null;
 }
