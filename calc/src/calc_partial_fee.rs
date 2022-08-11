20c5bdeb3bebdc93787a398fdcf8a645261c0bc5
tarikgul <tarik@parity.io>
2022-08-11 10:55:03 -0400

    remove impl, and unwraps, replace with Result

--- calc/src/calc_partial_fee.rs
+++ calc/src/calc_partial_fee.rs
@@ -15,113 +15,100 @@
 use sp_arithmetic::Perbill;
 use wasm_bindgen::prelude::*;
 
+/// Uses the following formula to calculate an extrinsics `partial_fee` (ie the total fee
+/// minus any tip).
+///
+/// ```
+/// partial_fee = base_fee + len_fee + ((adjusted_weight_fee/estimated_weight)*actual_weight)
+/// ```
+///
+/// Where:
+/// - `base_fee` is a fixed base fee to include some transaction in a block. It accounts
+///   for the work needed to verify the signature and such and is constant for any tx.
+/// - `len_fee` is a fee paid based on the size (length in bytes) of the transaction. Longer
+///   transactions require more storage.
+/// - `adjusted_weight_fee` is a fee that is itself `estimated_weight * targeted_fee_adjustment`.
+///   `targeted_fee_adjustment` is some adjustment made based on the network load and such, and is
+///   an opaque internal value we have no access to.
+/// - `estimated_weight` is the "pre-dispatch" weight of the transaction. It's set based on the cost
+///   of processing the transaction on reference hardware.
+/// - `actual_weight` is the weight that is found in the `ExtrinsicSuccess` event for the extrinsic in
+///   a block (it's just called `weight` in the event), and is often the same as `estimated_weight`,
+///   but the node has the opportunity to change it to whatever it likes, I think.
+///
+/// The RPC endpoint `payment_queryFeeDetails` returns `base_fee`, `len_fee` and `adjusted_weight_fee`/
+/// The RPC endpoint `payment_queryInfo` returns `estimated_weight` (called `weight` in the response), and
+/// a `partialFee` value, which is our best guess at the inclusion fee for the tx without actually submitting
+/// it and seeing whether the node changes the weight/decides not to take a fee at all.
+///
+/// To get the correct values for some extrinsic from both endpoints, provide the extrinsic bytes, and the
+/// block number **one before the block it made it into** (eg if the extrinsic was in block 100, you'd use
+/// block 99 as an argument). This is very important.
+///
+/// Once you've called these endpoints, access the `ExtrinsicSuccess` event to find the `actual_weight`, but
+/// also a `paysFee` value which signals whether the extrinsic actually incurred a fee at all or not (a node
+/// has the opportunity to refund the fee entirely if it likes by setting this).
+///
+/// With all of those values to hand, the equation above calculates the correct Fee. Why? Well, the basic
+/// way to calculate a pre-dispatch fee is:
+///
+/// ```
+/// partial_fee = base_fee + len_fee + adjusted_weight_fee
+/// ```
+///
+/// We can do this from just the RPC methods. But then once it's in a block, we need to swap out the weight used
+/// to calculate that `adjusted_weight_fee` with the actual weight that was used from the `ExtrinsicSuccess` event.
+/// In the end, the maths is simple and gathering the details needed is the main difficulty. We do this all in
+/// Rust simply to limit any precision loss.
 #[wasm_bindgen]
-#[derive(Debug, PartialEq)]
-pub struct CalcPartialFee {
-    partial_fee: u128,
+pub fn calc_partial_fee(
+    base_fee: &str,
+    len_fee: &str,
+    adjusted_weight_fee: &str,
+    estimated_weight: &str,
+    actual_weight: &str
+) -> Result<String, JsError> {
+    let base_fee: u128 = base_fee.parse()?;
+    let len_fee: u128 = len_fee.parse()?;
+    let adjusted_weight_fee: u128 = adjusted_weight_fee.parse()?;
+    let estimated_weight: u128 = estimated_weight.parse()?;
+    let actual_weight: u128 = actual_weight.parse()?;
+
+    let partial_fee = calc_raw(
+        base_fee,
+        len_fee,
+        adjusted_weight_fee,
+        estimated_weight,
+        actual_weight
+    );
+
+    Ok(partial_fee.to_string())
 }
 
-#[wasm_bindgen]
-impl CalcPartialFee {
-    /// Uses the following formula to calculate an extrinsics `partial_fee` (ie the total fee
-    /// minus any tip).
-    ///
-    /// ```
-    /// partial_fee = base_fee + len_fee + ((adjusted_weight_fee/estimated_weight)*actual_weight)
-    /// ```
-    ///
-    /// Where:
-    /// - `base_fee` is a fixed base fee to include some transaction in a block. It accounts
-    ///   for the work needed to verify the signature and such and is constant for any tx.
-    /// - `len_fee` is a fee paid based on the size (length in bytes) of the transaction. Longer
-    ///   transactions require more storage.
-    /// - `adjusted_weight_fee` is a fee that is itself `estimated_weight * targeted_fee_adjustment`.
-    ///   `targeted_fee_adjustment` is some adjustment made based on the network load and such, and is
-    ///   an opaque internal value we have no access to.
-    /// - `estimated_weight` is the "pre-dispatch" weight of the transaction. It's set based on the cost
-    ///   of processing the transaction on reference hardware.
-    /// - `actual_weight` is the weight that is found in the `ExtrinsicSuccess` event for the extrinsic in
-    ///   a block (it's just called `weight` in the event), and is often the same as `estimated_weight`,
-    ///   but the node has the opportunity to change it to whatever it likes, I think.
-    ///
-    /// The RPC endpoint `payment_queryFeeDetails` returns `base_fee`, `len_fee` and `adjusted_weight_fee`/
-    /// The RPC endpoint `payment_queryInfo` returns `estimated_weight` (called `weight` in the response), and
-    /// a `partialFee` value, which is our best guess at the inclusion fee for the tx without actually submitting
-    /// it and seeing whether the node changes the weight/decides not to take a fee at all.
-    ///
-    /// To get the correct values for some extrinsic from both endpoints, provide the extrinsic bytes, and the
-    /// block number **one before the block it made it into** (eg if the extrinsic was in block 100, you'd use
-    /// block 99 as an argument). This is very important.
-    ///
-    /// Once you've called these endpoints, access the `ExtrinsicSuccess` event to find the `actual_weight`, but
-    /// also a `paysFee` value which signals whether the extrinsic actually incurred a fee at all or not (a node
-    /// has the opportunity to refund the fee entirely if it likes by setting this).
-    ///
-    /// With all of those values to hand, the equation above calculates the correct Fee. Why? Well, the basic
-    /// way to calculate a pre-dispatch fee is:
-    ///
-    /// ```
-    /// partial_fee = base_fee + len_fee + adjusted_weight_fee
-    /// ```
-    ///
-    /// We can do this from just the RPC methods. But then once it's in a block, we need to swap out the weight used
-    /// to calculate that `adjusted_weight_fee` with the actual weight that was used from the `ExtrinsicSuccess` event.
-    /// In the end, the maths is simple and gathering the details needed is the main difficulty. We do this all in
-    /// Rust simply to limit any precision loss.
-    pub fn calc(
-        base_fee: &str,
-        len_fee: &str,
-        adjusted_weight_fee: &str,
-        estimated_weight: &str,
-        actual_weight: &str
-    ) -> CalcPartialFee {
-        let base_fee = new_u128(base_fee);
-        let len_fee = new_u128(len_fee);
-        let adjusted_weight_fee = new_u128(adjusted_weight_fee);
-        let estimated_weight = new_u128(estimated_weight);
-        let actual_weight = new_u128(actual_weight);
+fn calc_raw(
+    base_fee: u128,
+    len_fee: u128,
+    adjusted_weight_fee: u128,
+    estimated_weight: u128,
+    actual_weight: u128
+) -> u128 {
+    // calculate new adjusted_weight_fee, trying to maintain precision:
+    let adjusted_weight_fee = Perbill::from_rational(estimated_weight, actual_weight) * adjusted_weight_fee;
 
-        let partial_fee = Self::calc_raw(
-            base_fee,
-            len_fee,
-            adjusted_weight_fee,
-            estimated_weight,
-            actual_weight
-        );
-
-        Self {
-            partial_fee
-        }
-    }
-
-    /// Return the calculated partial fee as a string, to avoid any precision
-    /// loss as it crosses the boundary back to JS land.
-    pub fn partial_fee_as_string(&self) -> String {
-        self.partial_fee.to_string()
-    }
-
-    fn calc_raw(
-        base_fee: u128,
-        len_fee: u128,
-        adjusted_weight_fee: u128,
-        estimated_weight: u128,
-        actual_weight: u128
-    ) -> u128 {
-        // calculate new adjusted_weight_fee, trying to maintain precision:
-        let adjusted_weight_fee = Perbill::from_rational(estimated_weight, actual_weight) * adjusted_weight_fee;
-
-        // add the fee components together to get the partial/inclusion fee:
-        base_fee.saturating_add(len_fee).saturating_add(adjusted_weight_fee)
-    }
-}
-
-fn new_u128(inner: &str) -> u128 {
-    inner.parse().expect("string is not a valid u128")
+    // add the fee components together to get the partial/inclusion fee:
+    base_fee.saturating_add(len_fee).saturating_add(adjusted_weight_fee)
 }
 
 #[cfg(test)]
 mod test_fees {
-    use super::CalcPartialFee;
+    use super::*;
+
+    fn unwrap<T>(val: Result<T, JsError>) -> T {
+        match val {
+            Ok(v) => v,
+            Err(_e) => panic!("Cannot unwrap result")
+        }
+    }
 
     // Fee calculation example 1:
     //
@@ -158,9 +145,9 @@ mod test_fees {
         // so we know this was the total fee amount taken. Also is the
         // actual partialFee, so we are really just testing that our calc
         // call returns this, too.
-        let expected_partial_fee = 1611916814889018;
+        let expected_partial_fee = "1611916814889018";
 
-        let actual_partial_fee = CalcPartialFee::calc(
+        let actual_partial_fee = calc_partial_fee(
             &base_fee.to_string(),
             &len_fee.to_string(),
             &adjusted_weight_fee.to_string(),
@@ -168,7 +155,7 @@ mod test_fees {
             &actual_weight.to_string()
         );
 
-        assert_eq!(expected_partial_fee, actual_partial_fee.partial_fee);
+        assert_eq!(expected_partial_fee, unwrap(actual_partial_fee));
     }
 
     // Fee calculation example 2:
@@ -201,9 +188,9 @@ mod test_fees {
         // From https://shiden.subscan.io/extrinsic/1820341-2
         // Also seen in Balances.Withdraw event associated with the tx,
         // so we know this was the total fee amount taken.
-        let expected_partial_fee = 1611917528885264;
+        let expected_partial_fee = "1611917528885264";
 
-        let actual_partial_fee = CalcPartialFee::calc(
+        let actual_partial_fee = calc_partial_fee(
             &base_fee.to_string(),
             &len_fee.to_string(),
             &adjusted_weight_fee.to_string(),
@@ -211,7 +198,7 @@ mod test_fees {
             &actual_weight.to_string()
         );
 
-        assert_eq!(expected_partial_fee, actual_partial_fee.partial_fee);
+        assert_eq!(expected_partial_fee, unwrap(actual_partial_fee));
     }
 
     // Fee Calculation Example 3:
@@ -244,9 +231,9 @@ mod test_fees {
         // From https://acala.subscan.io/extrinsic/1285857-2
         // Also seen in Balances.Withdraw event associated with the tx,
         // so we know this was the total fee amount taken.
-        let expected_partial_fee = 2490128143;
+        let expected_partial_fee = "2490128143";
 
-        let actual_fee = CalcPartialFee::calc(
+        let actual_partial_fee = calc_partial_fee(
             &base_fee.to_string(), 
             &len_fee.to_string(), 
             &adjusted_weight_fee.to_string(), 
@@ -254,6 +241,6 @@ mod test_fees {
             &actual_weight.to_string()
         );
 
-        assert_eq!(expected_partial_fee, actual_fee.partial_fee);
+        assert_eq!(expected_partial_fee, unwrap(actual_partial_fee));
     }
 }
