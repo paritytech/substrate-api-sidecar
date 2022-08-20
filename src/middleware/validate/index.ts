61cb2dd8f1b49dccd025461a596eea5dd9099818
tarikgul <tarik@parity.io>
2022-08-19 21:37:57 -0400

    export middleware

--- src/middleware/validate/index.ts
+++ src/middleware/validate/index.ts
@@ -15,3 +15,4 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 export { validateAddressMiddleware as validateAddress } from './validateAddressMiddleware';
+export { validateBooleanMiddleware as validateBoolean } from './validateBooleanMiddleware';
