9c2effb358bbcd483e8c04c51f22e1988b3855fd
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2023-01-25 09:54:03 -0500

    feat: add fileTransport (#1189)
    * feat: add fileTransport for local logging
    
    * lint
    
    * add env vars to SidecarConfig
    
    * add logic to transport logger
    
    * adjust logging
    
    * update log config
    
    * add WRITE_MAX_FILE_SIZE and WRITE_MAX_FILES
    
    * update the docs
    
    * cleanup some grumbles
    
    * update readme

---
+++ src/logging/transports/fileTransport.ts
@@ -0,0 +1,15 @@
+import { transports } from 'winston';
+
+import { SidecarConfig } from '../../SidecarConfig';
+
+export const fileTransport = (
+	fileName: string
+): transports.FileTransportInstance => {
+	return new transports.File({
+		level: SidecarConfig.config.LOG.LEVEL,
+		filename: `${SidecarConfig.config.LOG.WRITE_PATH}/${fileName}`,
+		handleExceptions: true,
+		maxsize: SidecarConfig.config.LOG.WRITE_MAX_FILE_SIZE,
+		maxFiles: SidecarConfig.config.LOG.WRITE_MAX_FILES,
+	});
+};
