38a842d246386c529bf594e6bd9dd5fec3cb7c52
marshacb <cameron.marshall12@gmail.com>
2023-01-30 16:23:18 -0500

    update docs for consts endpoint

--- src/chains-config/statemineControllers.ts
+++ src/chains-config/statemineControllers.ts
@@ -31,6 +31,7 @@ export const statemineControllers: ControllerConfig = {
 		'NodeTransactionPool',
 		'NodeVersion',
 		'PalletsAssets',
+		'PalletsConsts',
 		'PalletsErrors',
 		'RuntimeCode',
 		'RuntimeMetadata',
