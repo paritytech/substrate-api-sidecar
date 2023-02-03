2172d26acaa46522a9ade81f63a1af88d1087014
tarikgul <tarik@parity.io>
2023-02-03 15:36:51 -0500

    add license

--- benchmarks/accountsVestingInfo/accountsVestingInfo.lua
+++ benchmarks/accountsVestingInfo/accountsVestingInfo.lua
@@ -1,3 +1,19 @@
+-- Copyright 2017-2023 Parity Technologies (UK) Ltd.
+-- This file is part of Substrate API Sidecar.
+--
+-- Substrate API Sidecar is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 local accountsVestingInfo = {
     '15aKvwRqGVAwuBMaogtQXhuz9EQqUWsZJSAzomyb5xYwgBXA/vesting-info?at=1448', -- v0
     '123PewK4ZYcX7Do8PKzP4KyYbLKMQAAA3EhhZcnBDrxAuidt/vesting-info?at=10254', -- v0
