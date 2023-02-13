c8f638a8d77a0f579d0804caaeb4e6a977c99f7a
marshacb <cameron.marshall12@gmail.com>
2023-02-13 09:36:24 -0500

    yarn lint

--- src/services/AbstractPalletsService.ts
+++ src/services/AbstractPalletsService.ts
@@ -145,7 +145,7 @@ export abstract class AbstractPalletsService extends AbstractService {
 		if (isValidPalletIndex === false && isValidPalletName === false) {
 			throw new BadRequest(
 				`"${palletId}" was not recognized as a queryable pallet.`
-			  );
+			);
 		} else if (!palletMeta || palletIdx === undefined || palletIdx < 0) {
 			throw new BadRequest(
 				`no queryable ${metadataFieldType} items found for palletId "${palletId}"`
