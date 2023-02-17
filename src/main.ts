a0005591c6675f26a2b14e632ba1c3ebfc4f3ac2
tarikgul <tarik@parity.io>
2023-02-16 23:07:49 -0500

    lint

--- src/main.ts
+++ src/main.ts
@@ -90,7 +90,7 @@ async function main() {
 
 	// Start the server
 	const server = app.listen();
-	
+
 	server.keepAliveTimeout = config.EXPRESS.KEEP_ALIVE_TIMEOUT;
 	server.headersTimeout = config.EXPRESS.KEEP_ALIVE_TIMEOUT + 5;
 }
