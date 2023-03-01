4836150e057c5a5c7b5da50fd83046bc15f823c5
alvicsam <alvicsam@gmail.com>
2023-03-01 17:16:31 +0100

    fix naming

--- scripts/ci/benchmarks/push_benchmark_results.sh
+++ scripts/ci/benchmarks/push_benchmark_results.sh
@@ -15,16 +15,16 @@ BENCHMARKS=$(cat ${RESULT_FILE} | grep Result | awk '{print $3}')
 
 for benchmark in ${BENCHMARKS}
 do
-    # /accounts/{accountId}/balance-info: -> /accounts/(accountId)/balance-info
-    benchmark_name=$(echo ${benchmark} | cut -d ":" -f1 | tr "{" "(" | tr "}" ")" )
+    # "/accounts/{accountId}/balance-info:" -> "accounts-{accountId}-balance-info"
+    benchmark_name=$(echo ${benchmark} | cut -d ":" -f1 | sed 's/\///' | sed 's/\//-/g' )
     result=$(cat ${RESULT_FILE} | grep -A 20 ${benchmark} | grep "Avg RequestTime(Latency)" | awk '{print $3}')
     unit=${result: -2}
     result_value=${result::-2}
     echo "--------------------------------------------------------------------------"
     echo "Sending metrics for ${benchmark_name} with result: ${result_value} ${unit}"
     echo "Sending common metric"
-    push_bench_result -t common -p $CI_PROJECT_NAME -n sidecarb -r ${result_value} -u ${unit} -s ${PROMETHEUS_URL}
+    push_bench_result -t common -p $CI_PROJECT_NAME -n ${benchmark_name} -r ${result_value} -u ${unit} -s ${PROMETHEUS_URL}
     echo "Sending specific metric"
-    push_bench_result -t specific -p $CI_PROJECT_NAME -n sidecar -r ${result_value} -l 'commit="'${CI_COMMIT_SHORT_SHA}'",benchmark_name="'${benchmark_name}'"' -u ${unit} -s ${PROMETHEUS_URL}
+    push_bench_result -t specific -p $CI_PROJECT_NAME -n ${benchmark_name} -r ${result_value} -l 'commit="'${CI_COMMIT_SHORT_SHA}'"' -u ${unit} -s ${PROMETHEUS_URL}
     echo "--------------------------------------------------------------------------"
 done
