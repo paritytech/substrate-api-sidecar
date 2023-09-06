e87cede29c5caedb864aad2fc4320ed190c2509b
Dominique <dominique@imodworks.io>
2023-09-06 11:44:21 +0200

    Update src/services/pallets/PalletsForeignAssetsService.ts
    Co-authored-by: Tarik Gul <47201679+TarikGul@users.noreply.github.com>

--- src/services/pallets/PalletsForeignAssetsService.ts
+++ src/services/pallets/PalletsForeignAssetsService.ts
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { ApiPromise } from '@polkadot/api';
+import type { ApiPromise } from '@polkadot/api';
 import type { BlockHash, AssetMetadata } from '@polkadot/types/interfaces';
 import type { AnyJson } from '../../types/polkadot-js';
 
