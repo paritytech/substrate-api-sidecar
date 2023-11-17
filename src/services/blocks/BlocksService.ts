ed04caac35677727b04fa816963ea45f56aeefeb
tarikgul <tariksnow37@gmail.com>
2023-11-17 15:33:50 -0500

    ignore unsafe enum comparison

--- src/services/blocks/BlocksService.ts
+++ src/services/blocks/BlocksService.ts
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+/* eslint-disable @typescript-eslint/no-unsafe-enum-comparison */
+
 import { ApiPromise } from '@polkadot/api';
 import { ApiDecoration } from '@polkadot/api/types';
 import { extractAuthor } from '@polkadot/api-derive/type/util';
