c3d9d012762f5c93fca3d274fa08388d70296737
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2024-01-05 11:00:18 -0500

    perf: add additional high load benchmarks for `/blocks` (#1372)

---
+++ benchmarks/blocksBlockId/highLoadBlocks.lua
@@ -0,0 +1,50 @@
+-- Copyright 2017-2023 Parity Technologies (UK) Ltd.
+-- This file is part of Substrate API Sidecar.
+--
+-- Substrate API Sidecar is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+local highLoadBlocks = {
+    '18687076', -- 414 extrinsics
+    '18687064', -- 404 extrinsics
+    '18687053', -- 402 extrinsics
+    '18687063', -- 435 extrinsics
+    '18687111', -- 460 extrinsics
+    '18687104', -- 274 extrinsics
+    '18687099', -- 288 extrinsics
+    '18687162', -- 436 extrinsics
+    '18687150', -- 409 extrinsics
+    '18687146', -- 358 extrinsics
+    '18687186', -- 416 extrinsics
+    '18687181', -- 306 extrinsics
+    '18687219', -- 458 extrinsics
+    '18687214', -- 292 extrinsics
+    '18687212', -- 341 extrinsics
+    '18687264', -- 401 extrinsics
+    '18687255', -- 437 extrinsics
+    '18687246', -- 276 extrinsics
+    '18687338', -- 488 extrinsics
+    '18687370', -- 512 extrinsics
+    '18687387', -- 484 extrinsics
+    '18687436', -- 352 extrinsics
+    '18687425', -- 372 extrinsics
+    '18687507', -- 402 extrinsics
+    '18687526', -- 266 extrinsics
+    '18687604', -- 348 extrinsics
+    '18687639', -- 489 extrinsics
+    '18687630', -- 524 extrinsics
+    '18687692', -- 377 extrinsics
+    '18687712', -- 349 extrinsics
+}
+
+return highLoadBlocks
