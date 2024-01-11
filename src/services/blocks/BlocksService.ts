af2b13d5e583c42adc1dc02bbbe3b6c701e7072a
Imod7 <dominique@imodworks.io>
2024-01-11 16:38:10 +0100

    removed 2 options from the key

--- src/services/blocks/BlocksService.ts
+++ src/services/blocks/BlocksService.ts
@@ -112,15 +112,9 @@ export class BlocksService extends AbstractService {
 	): Promise<IBlock> {
 		const { api } = this;
 
-		// Create a key for the cache that is a concatenation of the block hash and all the query params/options included in the request
+		// Create a key for the cache that is a concatenation of the block hash and all the query params included in the request
 		const cacheKey =
-			hash.toString() +
-			Number(eventDocs) +
-			Number(extrinsicDocs) +
-			Number(checkFinalized) +
-			Number(queryFinalizedHead) +
-			Number(omitFinalizedTag) +
-			Number(noFees);
+			hash.toString() + Number(eventDocs) + Number(extrinsicDocs) + Number(checkFinalized) + Number(noFees);
 
 		// Before making any api calls check the cache if the queried block exists
 		const isBlockCached = this.blockStore.get(cacheKey);
