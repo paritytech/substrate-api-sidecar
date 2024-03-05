96c9f11d7c4142c6f8a145eb932fb650941ade50
Dominique <dominique@imodworks.io>
2024-03-05 23:00:48 +0100

    chore(deps): update non-pjs deps (#1407)

--- src/chains-config/cache/lruCache.ts
+++ src/chains-config/cache/lruCache.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2022 Parity Technologies (UK) Ltd.
+// Copyright 2017-2024 Parity Technologies (UK) Ltd.
 // This file is part of Substrate API Sidecar.
 //
 // Substrate API Sidecar is free software: you can redistribute it and/or modify
@@ -14,14 +14,14 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import LRU from 'lru-cache';
+import { LRUCache } from 'lru-cache';
 
 import { IBlock } from '../../types/responses';
 
-export const initLRUCache = (): LRU<string, IBlock> => {
+export const initLRUCache = (): LRUCache<string, IBlock> => {
 	const config = { max: 2 };
 	// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-	const cache = new LRU(config) as LRU<string, IBlock>;
+	const cache = new LRUCache(config) as LRUCache<string, IBlock>;
 
 	return cache;
 };
