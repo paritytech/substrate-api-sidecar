2b1426b4b70858bc5282748efb9b3d0c662586e3
Imod7 <dominique@imodworks.io>
2024-10-16 10:51:58 +0200

    fix: rococo deprecation changes

--- src/test-helpers/registries/index.ts
+++ src/test-helpers/registries/index.ts
@@ -18,4 +18,3 @@ export * from './assetHubKusamaRegistry';
 export * from './assetHubWestendRegistry';
 export * from './kusamaRegistry';
 export * from './polkadotRegistry';
-export * from './rococoRegistry';
