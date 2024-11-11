624a666151a7f0dad5235b8037dadb54e0cdfc00
Imod7 <dominique@imodworks.io>
2024-11-11 19:39:28 +0100

    use of TransformableInfo type & casting to string

--- src/logging/transformers/nodeUtilFormat.ts
+++ src/logging/transformers/nodeUtilFormat.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2022 Parity Technologies (UK) Ltd.
+// Copyright 2017-2024 Parity Technologies (UK) Ltd.
 // This file is part of Substrate API Sidecar.
 //
 // Substrate API Sidecar is free software: you can redistribute it and/or modify
@@ -14,22 +14,21 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import { TransformableInfo } from 'logform';
 import { SPLAT } from 'triple-beam';
 import { format } from 'util';
 import * as winston from 'winston';
-
-import { ITransformableInfo } from '../../types/logging';
 
 /**
  * Console.log style formatting using node's `util.format`. We need this so we
  * can override console.{log, error, etc.} without issue.
  */
-export const nodeUtilFormat = winston.format((info: ITransformableInfo, _opts: unknown) => {
+export const nodeUtilFormat = winston.format((info: TransformableInfo) => {
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-	const args = info[SPLAT as unknown as string];
+	const args = info[SPLAT as unknown as string] as string[];
 	if (args) {
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-		info.message = format(info.message, ...args);
+		info.message = format(info.message as string, ...args);
 	}
 	return info;
 });
