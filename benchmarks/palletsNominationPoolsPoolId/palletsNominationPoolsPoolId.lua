4ca40b2b073c73a4434ff1e990912bc8dde293c0
tarikgul <tariksnow37@gmail.com>
2025-01-07 08:51:52 -0500

    chore(headers): 2025

--- benchmarks/palletsNominationPoolsPoolId/palletsNominationPoolsPoolId.lua
+++ benchmarks/palletsNominationPoolsPoolId/palletsNominationPoolsPoolId.lua
@@ -1,4 +1,4 @@
--- Copyright 2017-2023 Parity Technologies (UK) Ltd.
+-- Copyright 2017-2025 Parity Technologies (UK) Ltd.
 -- This file is part of Substrate API Sidecar.
 --
 -- Substrate API Sidecar is free software: you can redistribute it and/or modify
