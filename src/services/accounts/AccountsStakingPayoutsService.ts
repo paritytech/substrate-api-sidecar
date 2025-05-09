c425c99618bb8c8cac9e0a99f417bfdcbb150541
Filippo Vecchiato <filippo@parity.io>
2025-05-09 22:23:41 +0200

    passing tests for multichain for AccountsStakingPayouts

--- src/services/accounts/AccountsStakingPayoutsService.ts
+++ src/services/accounts/AccountsStakingPayoutsService.ts
@@ -305,8 +305,15 @@ export class AccountsStakingPayoutsService extends AbstractService {
 					// to fetch the `Rewards` event at that block.
 					nextEraStartBlock = era === 0 ? earlyErasBlockInfo[era + 1].start : earlyErasBlockInfo[era].start;
 				} else {
+					const RCApiPromise = ApiPromiseRegistry.getApiByType('relay');
+					if (!RCApiPromise?.length) {
+						throw new Error('Relay chain API not found');
+					}
+					const relayApi = RCApiPromise[0].api;
 					const sessionDuration = historicApi.consts.staking.sessionsPerEra.toNumber();
-					const epochDuration = historicApi.consts.babe.epochDuration.toNumber();
+					const epochDuration = relayApi
+						? relayApi.consts.babe.epochDuration.toNumber()
+						: historicApi.consts.babe.epochDuration.toNumber();
 					eraDurationInBlocks = sessionDuration * epochDuration;
 				}
 				const nextEraStartBlockHash: BlockHash = await this.api.rpc.chain.getBlockHash(nextEraStartBlock);
