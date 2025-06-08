c40d910aae77f53fc66d6310500f90472ea30923
Filippo Vecchiato <filippo@parity.io>
2025-06-08 11:04:52 +0200

    fix wrong import type

--- src/services/AHM.spec.ts
+++ src/services/AHM.spec.ts
@@ -1,18 +1,3 @@
-import { setupContext } from '@acala-network/chopsticks-testing';
-
 describe('AHM', () => {
-	it('should have correct initial state', async () => {
-		const westendAH = await setupContext({
-			endpoint: 'wss://westend-asset-hub-rpc.polkadot.io',
-		});
-
-		const westendRC = await setupContext({
-			endpoint: 'wss://westend-rpc.polkadot.io',
-		});
-
-		console.log(westendAH.api.query);
-		console.log(westendRC.api.query);
-
-		// import services for staking, and then make sure they are running properly
-	}, 5000000);
+	test.todo('should have correct initial state');
 });
