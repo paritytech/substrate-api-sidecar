9ac318d42d10053c2721360be62fb756b7ae0099
Eugenio Paluello <eugenio@parity.io>
2025-11-11 18:00:13 +0100

    benchmark CI parameters

--- benchmarks/transactionMaterial/init.sh
+++ benchmarks/transactionMaterial/init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wrk -d$WRK_TIME_LENGTH -t4 -c6 --timeout 120s --latency -s ./benchTransactionMaterial.lua http://127.0.0.1:8080
+wrk -d${WRK_TIME_LENGTH:-1m} -t${WRK_THREADS:-4} -c${WRK_CONNECTIONS:-6} --timeout ${WRK_TIMEOUT:-120s} --latency -s ./benchTransactionMaterial.lua http://127.0.0.1:8080
