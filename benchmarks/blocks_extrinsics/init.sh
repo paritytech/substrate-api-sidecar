827da6a98c038262915441b413e27b6c5ad5bce9
Dominique <dominique@imodworks.io>
2026-04-27 18:43:30 +0200

    fix: finish benchmarks migration and remove init.sh from benchmark folders (#1954)

--- benchmarks/blocks_extrinsics/init.sh
+++
@@ -1,3 +0,0 @@
-#\!/bin/sh
-
-wrk -d$WRK_TIME_LENGTH -t4 -c6 --timeout 120s --latency -s ./blocks_extrinsics.lua http://127.0.0.1:8080
--- benchmarks/init.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-for d in ./*/ ; do (cd "$d" && export WRK_TIME_LENGTH=30s; sh init.sh); done
